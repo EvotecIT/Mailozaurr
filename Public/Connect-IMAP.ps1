54db363497b492ac99aea97ef4f9cdbf29b79ffa
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2020-04-22 00:10:39 +0200

    Added IMAP Connect/Disconnect

---
+++ Public/Connect-IMAP.ps1
@@ -0,0 +1,104 @@
+﻿function Connect-IMAP {
+    [cmdletBinding()]
+    param(
+        [Parameter(Mandatory)][string] $Server,
+        [int] $Port = '993',
+        [Parameter(ParameterSetName = 'ClearText', Mandatory)][string] $UserName,
+        [Parameter(ParameterSetName = 'ClearText', Mandatory)][string] $Password,
+        [Parameter(ParameterSetName = 'Credential', Mandatory)][System.Management.Automation.PSCredential] $Credential,
+        [MailKit.Security.SecureSocketOptions] $Options = [MailKit.Security.SecureSocketOptions]::Auto,
+        [int] $TimeOut = 120000
+    )
+
+    $Client = [MailKit.Net.Imap.ImapClient]::new()
+    try {
+        $Client.Connect($Server, $Port, $Options)
+    } catch {
+        Write-Warning "Connect-IMAP - Unable to connect $($_.Exception.Message)"
+        return
+    }
+
+    <#
+    void Connect(string host, int port, MailKit.Security.SecureSocketOptions options, System.Threading.CancellationToken cancellationToken)
+    void Connect(System.Net.Sockets.Socket socket, string host, int port, MailKit.Security.SecureSocketOptions options, System.Threading.CancellationToken cancellationToken)
+    void Connect(System.IO.Stream stream, string host, int port, MailKit.Security.SecureSocketOptions options, System.Threading.CancellationToken cancellationToken)
+    void Connect(uri uri, System.Threading.CancellationToken cancellationToken)
+    void Connect(string host, int port, bool useSsl, System.Threading.CancellationToken cancellationToken)
+    void IMailService.Connect(string host, int port, bool useSsl, System.Threading.CancellationToken cancellationToken)
+    void IMailService.Connect(string host, int port, MailKit.Security.SecureSocketOptions options, System.Threading.CancellationToken cancellationToken)
+    void IMailService.Connect(System.Net.Sockets.Socket socket, string host, int port, MailKit.Security.SecureSocketOptions options, System.Threading.CancellationToken cancellationToken)
+    void IMailService.Connect(System.IO.Stream stream, string host, int port, MailKit.Security.SecureSocketOptions options, System.Threading.CancellationToken cancellationToken)
+    #>
+    if ($Client.TimeOut -ne $TimeOut) {
+        $Client.TimeOut = $Timeout
+    }
+    if ($Client.IsConnected) {
+        if ($UserName -and $Password) {
+            try {
+                $Client.Authenticate($UserName, $Password)
+            } catch {
+                Write-Warning "Connect-IMAP - Unable to authenticate $($_.Exception.Message)"
+                return
+            }
+        } else {
+            try {
+                $Client.Authenticate($Credential)
+            } catch {
+                Write-Warning "Connect-IMAP - Unable to authenticate $($_.Exception.Message)"
+                return
+            }
+        }
+    } else {
+        return
+    }
+    if ($Client.IsAuthenticated) {
+        [ordered] @{
+            Uri                      = $Client.SyncRoot.Uri                      #: pops: / / pop.gmail.com:995 /
+            AuthenticationMechanisms = $Client.SyncRoot.AuthenticationMechanisms #: { }
+            Capabilities             = $Client.SyncRoot.Capabilities             #: Expire, LoginDelay, Pipelining, ResponseCodes, Top, UIDL, User
+            Stream                   = $Client.SyncRoot.Stream                   #: MailKit.Net.Pop3.Pop3Stream
+            State                    = $Client.SyncRoot.State                    #: Transaction
+            IsConnected              = $Client.SyncRoot.IsConnected              #: True
+            ApopToken                = $Client.SyncRoot.ApopToken                #:
+            ExpirePolicy             = $Client.SyncRoot.ExpirePolicy             #: 0
+            Implementation           = $Client.SyncRoot.Implementation           #:
+            LoginDelay               = $Client.SyncRoot.LoginDelay               #: 300
+            IsAuthenticated          = $Client.IsAuthenticated
+            IsSecure                 = $Client.IsSecure
+            Data                     = $Client
+            Count                    = $Client.Count
+        }
+    }
+    <#
+    void Authenticate(MailKit.Security.SaslMechanism mechanism, System.Threading.CancellationToken cancellationToken)
+    void Authenticate(System.Text.Encoding encoding, System.Net.ICredentials credentials, System.Threading.CancellationToken cancellationToken)
+    void Authenticate(System.Net.ICredentials credentials, System.Threading.CancellationToken cancellationToken)
+    void Authenticate(System.Text.Encoding encoding, string userName, string password, System.Threading.CancellationToken cancellationToken)
+    void Authenticate(string userName, string password, System.Threading.CancellationToken cancellationToken)
+    void IMailService.Authenticate(System.Net.ICredentials credentials, System.Threading.CancellationToken cancellationToken)
+    void IMailService.Authenticate(System.Text.Encoding encoding, System.Net.ICredentials credentials, System.Threading.CancellationToken cancellationToken)
+    void IMailService.Authenticate(System.Text.Encoding encoding, string userName, string password, System.Threading.CancellationToken cancellationToken)
+    void IMailService.Authenticate(string userName, string password, System.Threading.CancellationToken cancellationToken)
+    void IMailService.Authenticate(MailKit.Security.SaslMechanism mechanism, System.Threading.CancellationToken cancellationToken)
+    #>
+
+    <#
+    -------------------
+    System.Threading.Tasks.Task AuthenticateAsync(MailKit.Security.SaslMechanism mechanism, System.Threading.CancellationToken cancellationToken)
+    System.Threading.Tasks.Task AuthenticateAsync(System.Text.Encoding encoding, System.Net.ICredentials credentials, System.Threading.CancellationToken cancellati
+    onToken)
+    System.Threading.Tasks.Task AuthenticateAsync(System.Net.ICredentials credentials, System.Threading.CancellationToken cancellationToken)
+    System.Threading.Tasks.Task AuthenticateAsync(System.Text.Encoding encoding, string userName, string password, System.Threading.CancellationToken cancellationT
+    oken)
+    System.Threading.Tasks.Task AuthenticateAsync(string userName, string password, System.Threading.CancellationToken cancellationToken)
+    System.Threading.Tasks.Task IMailService.AuthenticateAsync(System.Net.ICredentials credentials, System.Threading.CancellationToken cancellationToken)
+    System.Threading.Tasks.Task IMailService.AuthenticateAsync(System.Text.Encoding encoding, System.Net.ICredentials credentials, System.Threading.CancellationTok
+    en cancellationToken)
+    System.Threading.Tasks.Task IMailService.AuthenticateAsync(System.Text.Encoding encoding, string userName, string password, System.Threading.CancellationToken
+    cancellationToken)
+    System.Threading.Tasks.Task IMailService.AuthenticateAsync(string userName, string password, System.Threading.CancellationToken cancellationToken)
+    System.Threading.Tasks.Task IMailService.AuthenticateAsync(MailKit.Security.SaslMechanism mechanism, System.Threading.CancellationToken cancellationToken)
+    #>
+
+    #$Client.GetMessageSizes
+}
---
+++ Public/Disconnect-IMAP.ps1
@@ -0,0 +1,14 @@
+﻿function Disconnect-IMAP {
+    [cmdletBinding()]
+    param(
+        [System.Collections.IDictionary] $Client
+    )
+    if ($Client.Data) {
+        try {
+            $Client.Data.Disconnect($true)
+        } catch {
+            Write-Warning "Disconnect-IMAP - Unable to authenticate $($_.Exception.Message)"
+            return
+        }
+    }
+}
