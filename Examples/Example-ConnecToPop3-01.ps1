b28a95194e750d7c0eef1cf5131fed883c5bfc30
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2020-04-22 00:10:01 +0200

    Pop3 examples

---
+++ Examples/Example-ConnecToPop3-01.ps1
@@ -0,0 +1,9 @@
+﻿Import-Module $PSScriptRoot\..\Mailozaurr.psd1 -Force
+
+$UserName = 'Test@gmail.com'
+$Password = 'TextPassword'
+
+$Client = Connect-POP3 -Server 'pop.gmail.com' -Password $Password -UserName $UserName -Port 995 -Options Auto
+Get-POP3Message -Client $Client -Index 0 -Count 5
+Save-POP3Message -Client $Client -Index 6 -Path "$Env:UserProfile\Desktop\mail.eml"
+Disconnect-POP3 -Client $Client
---
+++ Examples/Example-ConnecToPop3-02.ps1
@@ -0,0 +1,8 @@
+﻿Import-Module $PSScriptRoot\..\Mailozaurr.psd1 -Force
+
+$Credentials = Get-Credential
+$Client = Connect-POP3 -Server 'pop.gmail.com' -Credential $Credentials -Port 995 -Options Auto
+
+Get-POP3Message -Client $Client -Index 0 -Count 5
+Save-POP3Message -Client $Client -Index 6 -Path "$Env:UserProfile\Desktop\mail.eml"
+Disconnect-POP3 -Client $Client
