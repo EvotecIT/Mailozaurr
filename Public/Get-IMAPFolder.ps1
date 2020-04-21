67a309325bff14fe23aaf6fdc72b17d768c92688
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2020-04-22 00:10:52 +0200

    Added IMAP folder list

---
+++ Public/Get-IMAPFolder.ps1
@@ -0,0 +1,16 @@
+﻿function Get-IMAPFolder {
+    [cmdletBinding()]
+    param(
+        [Parameter(Mandatory)][System.Collections.IDictionary] $Client,
+        [MailKit.FolderAccess] $FolderAccess = [MailKit.FolderAccess]::ReadOnly
+    )
+
+    $Folder = $Client.Data.Inbox
+    $null = $Folder.Open($FolderAccess)
+
+    Write-Verbose "Get-IMAPMessage - Total messages $($Folder.Count), Recent messages $($Folder.Recent)"
+    $Client.Messages = $Folder
+    $Client.Count = $Folder.Count
+    $Client.Recent = $Folder.Recent
+    $Client
+}
