9db220692bb4d9f0cf77e4a173ca839fa47e96a1
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2020-05-07 22:39:22 +0200

    Update to ImapMessage

---
+++ Public/Get-IMAPMessage.ps1
@@ -0,0 +1,13 @@
+﻿function Get-IMAPMessage {
+    [cmdletBinding()]
+    param(
+        [Parameter(Mandatory)][System.Collections.IDictionary] $Client,
+        [MailKit.FolderAccess] $FolderAccess = [MailKit.FolderAccess]::ReadOnly
+    )
+
+    $Folder = $Client.Data.Inbox
+    $null = $Folder.Open($FolderAccess)
+
+    Write-Verbose "Get-IMAPMessage - Total messages $($Folder.Count), Recent messages $($Folder.Recent)"
+    $Client.Folder = $Folder
+}
