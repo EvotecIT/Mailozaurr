c6cd393e2a5c92f793771c3572eeac682455c9c9
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-07-24 22:04:13 +0200

    Added DKIM

---
+++ Public/Find-DKIMRecord.ps1
@@ -0,0 +1,45 @@
+function Find-DKIMRecord {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName, ValueFromPipeline)][string[]] $DomainName,
+        [string[]] $Selector = "selector1",
+        [System.Net.IPAddress] $DnsServer,
+        [switch] $AsHashTable,
+        [switch] $AsObject
+    )
+    process {
+        foreach ($Domain in $DomainName) {
+            foreach ($S in $Selector) {
+                $Splat = @{
+                    Name        = "$S._domainkey.$Domain"
+                    Type        = "TXT"
+                    ErrorAction = "SilentlyContinue"
+                }
+                if ($DnsServer) {
+                    $Splat['Server'] = $DnsServer
+                }
+                $DNSRecord = Resolve-DnsName @Splat | Where-Object Strings -Match "DKIM1"
+                if (-not $AsObject) {
+                    $MailRecord = [ordered] @{
+                        Name     = $Domain
+                        Count    = $DNSRecord.Strings.Count
+                        Selector = "$Domain`:$S"
+                        DKIM     = $DNSRecord.Strings -join '; '
+                    }
+                } else {
+                    $MailRecord = [ordered] @{
+                        Name     = $Domain
+                        Count    = $DNSRecord.Strings.Count
+                        Selector = "$Domain`:$S"
+                        DKIM     = $DNSRecord.Strings -join '; '
+                    }
+                }
+                if ($AsHashTable) {
+                    $MailRecord
+                } else {
+                    [PSCustomObject] $MailRecord
+                }
+            }
+        }
+    }
+}
