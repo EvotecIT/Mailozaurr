0b28bd69999b70c32001e85d7a77377d4a477940
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-07-26 00:03:19 +0200

    Added Test-EmailAddress

---
+++ Public/Test-EmailAddress.ps1
@@ -0,0 +1,14 @@
+﻿function Test-EmailAddress {
+    [cmdletBinding()]
+    param(
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName, ValueFromPipeline, Position = 0)][string[]] $EmailAddress
+    )
+    process {
+        foreach ($Email in $EmailAddress) {
+            [PSCustomObject] @{
+                EmailAddress = $Email
+                IsValid      = [EmailValidation.EmailValidator]::Validate($Email)
+            }
+        }
+    }
+}
