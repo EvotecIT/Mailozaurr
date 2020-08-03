ea60bc234139f89a1e8e0a95ccd681eaca69e36c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-08-03 23:32:09 +0200

    Updated Examples for Querying DNS records

--- Examples/Example-QueryMailRecords-01.ps1
+++ Examples/Example-QueryMailRecords-01.ps1
@@ -4,7 +4,7 @@ Find-MxRecord -DomainName 'evotec.pl', 'evotec.xyz' | Format-Table *
 Find-DMARCRecord -DomainName 'evotec.pl', 'evotec.xyz' | Format-Table *
 Find-SPFRecord -DomainName 'evotec.pl', 'evotec.xyz' | Format-Table *
 Find-DKIMRecord -DomainName 'evotec.pl', 'evotec.xyz' | Format-Table *
-
+Find-DKIMRecord -DomainName 'evotec.pl', 'evotec.xyz' -Selector 'selector1' | Format-Table *
 
 <#
 Name       Count Preference TTL MX                                     IPAddress
