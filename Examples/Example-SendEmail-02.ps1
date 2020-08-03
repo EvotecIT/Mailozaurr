100144ae8e30216d1fe0eb3ab83c686a5975a915
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-08-03 22:45:40 +0200

    Updated examples

--- Examples/Example-SendEmail-02.ps1
+++ Examples/Example-SendEmail-02.ps1
@@ -1,15 +1,17 @@
 ﻿Import-Module $PSScriptRoot\..\Mailozaurr.psd1 -Force
 
+$UserNotify = 'Przemyslaw'
+
 $Body = EmailBody -FontFamily 'Calibri' -Size 15 {
-    EmailText -Text "Hello ", $UserNotify, "," -Color None, Blue, None -Verbose -LineBreak
-    EmailText -Text "Your password is due to expire in ", $PasswordExpiryDays, "days." -Color None, Green, None
+    EmailText -Text 'Hello ', $UserNotify, ',' -Color None, Blue, None -Verbose -LineBreak
+    EmailText -Text 'Your password is due to expire in ', $PasswordExpiryDays, 'days.' -Color None, Green, None
     EmailText -LineBreak
     EmailText -Text 'To change your password: '
     EmailText -Text '- press ', 'CTRL+ALT+DEL', ' -> ', 'Change a password...' -Color None, BlueViolet, None, Red
     EmailText -LineBreak
     EmailTextBox {
         'If you have forgotten your password and need to reset it, you can do this by clicking here. '
-        "In case of problems please contact the HelpDesk by visiting [Evotec Website](https://evotec.xyz) or by sending an email to Help Desk."
+        'In case of problems please contact the HelpDesk by visiting [Evotec Website](https://evotec.xyz) or by sending an email to Help Desk.'
     }
     EmailText -LineBreak
     EmailText -Text 'Alternatively you can always call ', 'Help Desk', ' at ', '+48 22 00 00 00' `
@@ -25,9 +27,9 @@ if (-not $MailCredentials) {
     $MailCredentials = Get-Credential
 }
 
-Send-EmailMessage -From @{ Name = "Przemysław Kłys"; Email = 'przemyslaw.klys@evotec.pl' } -To 'przemyslaw.klys@evotec.pl' `
+Send-EmailMessage -From @{ Name = 'Przemysław Kłys'; Email = 'przemyslaw.klys@test.pl' } -To 'przemyslaw.klys@test.pl' `
     -Server 'smtp.office365.com' -SecureSocketOptions Auto -Credential $MailCredentials -HTML $Body -DeliveryNotificationOption OnSuccess -Priority High `
     -Subject 'This is another test email'
 
-Send-MailMessage -To 'przemyslaw.klys@evotec.pl' -Subject 'Test' -Body 'test me' -SmtpServer 'smtp.office365.com' -From 'przemyslaw.klys@evotec.pl' `
-    -Attachments "$PSScriptRoot\..\Mailozaurr.psd1" -Encoding UTF8 -Cc 'przemyslaw.klys@evotec.pl'  -DeliveryNotificationOption OnSuccess -Priority High -Credential $MailCredentials -UseSsl -Port 587 -Verbose
+Send-MailMessage -To 'przemyslaw.klys@test.pl' -Subject 'Test' -Body 'test me' -SmtpServer 'smtp.office365.com' -From 'przemyslaw.klys@test.pl' `
+    -Attachments "$PSScriptRoot\..\Mailozaurr.psd1" -Encoding UTF8 -Cc 'przemyslaw.klys@test.pl' -DeliveryNotificationOption OnSuccess -Priority High -Credential $MailCredentials -UseSsl -Port 587 -Verbose
--- Examples/Example-SendEmail-OAuthO365.ps1
+++ Examples/Example-SendEmail-OAuthO365.ps1
@@ -3,8 +3,8 @@ Import-Module $PSScriptRoot\..\Mailozaurr.psd1 -Force
 $ClientID = '4c1197dd-53'
 $TenantID = 'ceb371f6-87'
 
-$oAuth2 = Connect-oAuthO365 -ClientID $ClientID -TenantID $TenantID
+$CredentialOAuth2 = Connect-oAuthO365 -ClientID $ClientID -TenantID $TenantID
 
-Send-EmailMessage -From @{ Name = "Przemysław Kłys"; Email = 'test@evotec.pl' } -To 'test@evotec.pl' `
+Send-EmailMessage -From @{ Name = 'Przemysław Kłys'; Email = 'test@evotec.pl' } -To 'test@evotec.pl' `
     -Server 'smtp.office365.com' -HTML $Body -Text $Text -DeliveryNotificationOption OnSuccess -Priority High `
-    -Subject 'This is another test email' -SecureSocketOptions Auto -oAuth $oAuth2
+    -Subject 'This is another test email' -SecureSocketOptions Auto -Credential $CredentialOAuth2 -oAuth2
