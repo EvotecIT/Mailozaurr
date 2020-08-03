42dbf8952f8fc4147b00b87253088438eb729ca4
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-08-03 20:49:28 +0200

    Updated example 1

--- Examples/Example-ConnecToPop3-02.ps1
+++ Examples/Example-ConnecToPop3-02.ps1
@@ -2,7 +2,6 @@
 
 $Credentials = Get-Credential
 $Client = Connect-POP3 -Server 'pop.gmail.com' -Credential $Credentials -Port 995 -Options Auto
-
-Get-POP3Message -Client $Client -Index 0 -Count 5
+Get-POP3Message -Client $Client -Index 0 -Count 5 | Format-Table
 Save-POP3Message -Client $Client -Index 6 -Path "$Env:UserProfile\Desktop\mail.eml"
 Disconnect-POP3 -Client $Client
