7b26f711393da0a0a005f9692d95dd5384283e96
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-08-03 15:25:58 +0200

    Update

--- Private/Connect-O365Graph.ps1
+++ Private/Connect-O365Graph.ps1
@@ -7,20 +7,20 @@ function Connect-O365Graph {
         [ValidateSet('https://manage.office.com', 'https://graph.microsoft.com')] $Resource = 'https://manage.office.com'
     )
     # https://dzone.com/articles/getting-access-token-for-microsoft-graph-using-oau-1
-    <#
-    $Scope = @(
-        #'https://outlook.office.com/IMAP.AccessAsUser.All',
-        # 'https://outlook.office.com/POP.AccessAsUser.All',
-        #'https://outlook.office.com/Mail.Send'
-        #'https://outlook.office.com/User.Read'
-    )
-    #>
+
+    #$Scope = @(
+    #'https://outlook.office.com/IMAP.AccessAsUser.All',
+    # 'https://outlook.office.com/POP.AccessAsUser.All',
+    #    'https://outlook.office.com/Mail.Send'
+    #    'https://outlook.office.com/User.Read'
+    #)
+
     $Body = @{
         grant_type    = 'client_credentials'
         resource      = $Resource
         client_id     = $ApplicationID
         client_secret = $ApplicationKey
-        # scope         = [System.Web.HttpUtility]::UrlEncode( $Scope)
+        scope         = [System.Web.HttpUtility]::UrlEncode( $Scope)
     }
     try {
         $Authorization = Invoke-RestMethod -Method Post -Uri "https://login.microsoftonline.com/$($TenantDomain)/oauth2/token" -Body $body -ErrorAction Stop
