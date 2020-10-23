d2a7974835cc5a0a51bed84c3c1225b1902ac4f5
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-10-23 20:39:38 +0200

    Update Find-SPFRecord

--- Public/Find-SPFRecord.ps1
+++ Public/Find-SPFRecord.ps1
@@ -1,8 +1,46 @@
 function Find-SPFRecord {
+    <#
+    .SYNOPSIS
+    Queries DNS to provide SPF information
+
+    .DESCRIPTION
+    Queries DNS to provide SPF information
+
+    .PARAMETER DomainName
+    Name/DomainName to query for SPF record
+
+    .PARAMETER DnsServer
+    Allows to choose DNS IP address to ask for DNS query. By default uses system ones.
+
+    .PARAMETER DNSProvider
+    Allows to choose DNS Provider that will be used for HTTPS based DNS query (Cloudlare or Google)
+
+    .PARAMETER AsHashTable
+    Returns Hashtable instead of PSCustomObject
+
+    .PARAMETER AsObject
+    Returns an object rather than string based represantation for name servers (for easier display purposes)
+
+    .EXAMPLE
+    # Standard way
+    Find-SPFRecord -DomainName 'evotec.pl', 'evotec.xyz' | Format-Table *
+
+    .EXAMPLE
+    # Https way via Cloudflare
+    Find-SPFRecord -DomainName 'evotec.pl', 'evotec.xyz' -DNSProvider Cloudflare | Format-Table *
+
+    .EXAMPLE
+    # Https way via Google
+    Find-SPFRecord -DomainName 'evotec.pl', 'evotec.xyz' -DNSProvider Google | Format-Table *
+
+    .NOTES
+    General notes
+    #>
     [CmdletBinding()]
     param (
         [Parameter(Mandatory, ValueFromPipelineByPropertyName, ValueFromPipeline, Position = 0)][Array]$DomainName,
         [string] $DnsServer,
+        [ValidateSet('Cloudflare', 'Google')][string] $DNSProvider,
         [switch] $AsHashTable,
         [switch] $AsObject
     )
@@ -21,11 +59,15 @@ function Find-SPFRecord {
                 Type        = 'txt'
                 ErrorAction = 'Stop'
             }
-            if ($DnsServer) {
-                $Splat['Server'] = $DnsServer
-            }
             try {
-                $DNSRecord = Resolve-DnsQuery @Splat -All
+                if ($DNSProvider) {
+                    $DNSRecord = Resolve-DnsQueryRest @Splat -All -DNSProvider $DnsProvider
+                } else {
+                    if ($DnsServer) {
+                        $Splat['Server'] = $DnsServer
+                    }
+                    $DNSRecord = Resolve-DnsQuery @Splat -All
+                }
                 $DNSRecordAnswers = $DNSRecord.Answers | Where-Object Text -Match 'spf1'
                 if (-not $AsObject) {
                     $MailRecord = [ordered] @{
