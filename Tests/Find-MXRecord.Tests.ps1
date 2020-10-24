d9bd1e29aa9b1d341e648d136fe43b49a4a5a9ce
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-10-24 17:33:09 +0200

    MX Tests

--- Tests/Find-MXRecord.Tests.ps1
+++ Tests/Find-MXRecord.Tests.ps1
@@ -1,7 +1,30 @@
 Describe 'Find-MxRecord' {
     It 'Given 2 domains it should return two records' {
-
         $DNS = Find-MxRecord -DomainName 'evotec.pl', 'evotec.xyz'
+        $DNS.Count | Should -Be 2
+        $DNS[0].Name | Should -Be 'evotec.pl'
+        $DNS[0].MX | Should -Be 'evotec-pl.mail.protection.outlook.com'
+        $DNS[1].Name | Should -Be 'evotec.xyz'
+        $DNS[1].MX | Should -Be 'evotec-xyz.mail.protection.outlook.com'
+    }
+    It 'Given 2 domains it should return two records using specific server' {
+        $DNS = Find-MxRecord -DomainName 'evotec.pl', 'evotec.xyz' -DnsServer 1.1.1.1
+        $DNS.Count | Should -Be 2
+        $DNS[0].Name | Should -Be 'evotec.pl'
+        $DNS[0].MX | Should -Be 'evotec-pl.mail.protection.outlook.com'
+        $DNS[1].Name | Should -Be 'evotec.xyz'
+        $DNS[1].MX | Should -Be 'evotec-xyz.mail.protection.outlook.com'
+    }
+    It 'Given 2 domains it should return two records using HTTPS GOOGLE' {
+        $DNS = Find-MxRecord -DomainName 'evotec.pl', 'evotec.xyz' -DNSProvider Google
+        $DNS.Count | Should -Be 2
+        $DNS[0].Name | Should -Be 'evotec.pl'
+        $DNS[0].MX | Should -Be 'evotec-pl.mail.protection.outlook.com'
+        $DNS[1].Name | Should -Be 'evotec.xyz'
+        $DNS[1].MX | Should -Be 'evotec-xyz.mail.protection.outlook.com'
+    }
+    It 'Given 2 domains it should return two records using HTTPS Cloudflare' {
+        $DNS = Find-MxRecord -DomainName 'evotec.pl', 'evotec.xyz' -DNSProvider Cloudflare
         $DNS.Count | Should -Be 2
         $DNS[0].Name | Should -Be 'evotec.pl'
         $DNS[0].MX | Should -Be 'evotec-pl.mail.protection.outlook.com'
