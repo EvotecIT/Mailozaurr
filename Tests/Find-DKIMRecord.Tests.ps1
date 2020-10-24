39a9a817d575aa409544ea126c6cecee8fedd3b9
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-10-24 23:23:36 +0200

    Update

---
+++ Tests/Find-DKIMRecord.Tests.ps1
@@ -0,0 +1,48 @@
+﻿Describe 'Find-DKIMRecord' {
+    It 'Given 2 domains it should return two records' {
+        $DNS = Find-DKIMRecord -DomainName 'evotec.pl', 'evotec.xyz'
+        $DNS.Count | Should -Be 2
+        $DNS[0].Name | Should -Be 'evotec.pl'
+        $DNS[0].DKIM | Should -Be 'v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCqrIpQkyykYEQbNzvHfgGsiYfoyX3b3Z6CPMHa5aNn/Bd8skLaqwK9vj2fHn70DA+X67L/pV2U5VYDzb5AUfQeD6NPDwZ7zLRc0XtX+5jyHWhHueSQT8uo6acMA+9JrVHdRfvtlQo8Oag8SLIkhaUea3xqZpijkQR/qHmo3GIfnQIDAQAB;'
+        $DNS[1].Name | Should -Be 'evotec.xyz'
+        $DNS[1].DKIM | Should -Be 'v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDhI50wSneU7XBAY5e/L0EYs7DtoEN2MV4S9nZt9LpSIliNvlOXM2hJ9N4ks1dga5HwnfLH8LCudOQD/m+Me8S8V+xQXEAd6uHFOKiZiup8VLor07eBs5A6OqmQ4hnJ0ioo54KHfhqRAHnC5e/oo26wuntHAzQsaTuQEIRPY8v1aQIDAQAB; n=1024,1449053169,1'
+        $DNS[0].Selector | Should -Be 'evotec.pl:selector1'
+        $DNS[1].Selector | Should -Be 'evotec.xyz:selector1'
+    }
+    It 'Given 2 domains it should return two records using specific server' {
+        $DNS = Find-DKIMRecord -DomainName 'evotec.pl', 'evotec.xyz' -DnsServer 1.1.1.1
+        $DNS.Count | Should -Be 2
+        $DNS[0].Name | Should -Be 'evotec.pl'
+        $DNS[0].DKIM | Should -Be 'v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCqrIpQkyykYEQbNzvHfgGsiYfoyX3b3Z6CPMHa5aNn/Bd8skLaqwK9vj2fHn70DA+X67L/pV2U5VYDzb5AUfQeD6NPDwZ7zLRc0XtX+5jyHWhHueSQT8uo6acMA+9JrVHdRfvtlQo8Oag8SLIkhaUea3xqZpijkQR/qHmo3GIfnQIDAQAB;'
+        $DNS[1].Name | Should -Be 'evotec.xyz'
+        $DNS[1].DKIM | Should -Be 'v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDhI50wSneU7XBAY5e/L0EYs7DtoEN2MV4S9nZt9LpSIliNvlOXM2hJ9N4ks1dga5HwnfLH8LCudOQD/m+Me8S8V+xQXEAd6uHFOKiZiup8VLor07eBs5A6OqmQ4hnJ0ioo54KHfhqRAHnC5e/oo26wuntHAzQsaTuQEIRPY8v1aQIDAQAB; n=1024,1449053169,1'
+        $DNS[0].QueryServer | Should -Be '1.1.1.1:53'
+        $DNS[1].QueryServer | Should -Be '1.1.1.1:53'
+        $DNS[0].Selector | Should -Be 'evotec.pl:selector1'
+        $DNS[1].Selector | Should -Be 'evotec.xyz:selector1'
+    }
+    It 'Given 2 domains it should return two records using HTTPS GOOGLE' {
+        $DNS = Find-DKIMRecord -DomainName 'evotec.pl', 'evotec.xyz' -DNSProvider Google
+        $DNS.Count | Should -Be 2
+        $DNS[0].Name | Should -Be 'evotec.pl'
+        $DNS[0].DKIM | Should -Be 'v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCqrIpQkyykYEQbNzvHfgGsiYfoyX3b3Z6CPMHa5aNn/Bd8skLaqwK9vj2fHn70DA+X67L/pV2U5VYDzb5AUfQeD6NPDwZ7zLRc0XtX+5jyHWhHueSQT8uo6acMA+9JrVHdRfvtlQo8Oag8SLIkhaUea3xqZpijkQR/qHmo3GIfnQIDAQAB;'
+        $DNS[1].Name | Should -Be 'evotec.xyz'
+        $DNS[1].DKIM | Should -Be 'v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDhI50wSneU7XBAY5e/L0EYs7DtoEN2MV4S9nZt9LpSIliNvlOXM2hJ9N4ks1dga5HwnfLH8LCudOQD/m+Me8S8V+xQXEAd6uHFOKiZiup8VLor07eBs5A6OqmQ4hnJ0ioo54KHfhqRAHnC5e/oo26wuntHAzQsaTuQEIRPY8v1aQIDAQAB; n=1024,1449053169,1'
+        $DNS[0].QueryServer | Should -Be 'dns.google.com'
+        $DNS[1].QueryServer | Should -Be 'dns.google.com'
+        $DNS[0].Selector | Should -Be 'evotec.pl:selector1'
+        $DNS[1].Selector | Should -Be 'evotec.xyz:selector1'
+    }
+    It 'Given 2 domains it should return two records using HTTPS Cloudflare' {
+        $DNS = Find-DKIMRecord -DomainName 'evotec.pl', 'evotec.xyz' -DNSProvider Cloudflare
+        $DNS.Count | Should -Be 2
+        $DNS[0].Name | Should -Be 'evotec.pl'
+        $DNS[0].DKIM | Should -Be 'v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCqrIpQkyykYEQbNzvHfgGsiYfoyX3b3Z6CPMHa5aNn/Bd8skLaqwK9vj2fHn70DA+X67L/pV2U5VYDzb5AUfQeD6NPDwZ7zLRc0XtX+5jyHWhHueSQT8uo6acMA+9JrVHdRfvtlQo8Oag8SLIkhaUea3xqZpijkQR/qHmo3GIfnQIDAQAB;'
+        $DNS[1].Name | Should -Be 'evotec.xyz'
+        $DNS[1].DKIM | Should -Be 'v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDhI50wSneU7XBAY5e/L0EYs7DtoEN2MV4S9nZt9LpSIliNvlOXM2hJ9N4ks1dga5HwnfLH8LCudOQD/m+Me8S8V+xQXEAd6uHFOKiZiup8VLor07eBs5A6OqmQ4hnJ0ioo54KHfhqRAHnC5e/oo26wuntHAzQsaTuQEIRPY8v1aQIDAQAB; n=1024,1449053169,1'
+        $DNS[0].QueryServer | Should -Be 'cloudflare-dns.com'
+        $DNS[1].QueryServer | Should -Be 'cloudflare-dns.com'
+        $DNS[0].Selector | Should -Be 'evotec.pl:selector1'
+        $DNS[1].Selector | Should -Be 'evotec.xyz:selector1'
+    }
+}
---
+++ Tests/Find-DMARCRecord.Tests.ps1
@@ -0,0 +1,40 @@
+﻿Describe 'Find-DMARCRecord' {
+    It 'Given 2 domains it should return two records' {
+        $DNS = Find-DMARCRecord -DomainName 'evotec.pl', 'evotec.xyz'
+        $DNS.Count | Should -Be 2
+        $DNS[0].Name | Should -Be 'evotec.pl'
+        $DNS[0].DMARC | Should -Be 'v=DMARC1;p=none;rua=mailto:dmarc_agg@vali.email;sp=none;aspf=s;'
+        $DNS[1].Name | Should -Be 'evotec.xyz'
+        $DNS[1].DMARC | Should -Be ''
+    }
+    It 'Given 2 domains it should return two records using specific server' {
+        $DNS = Find-DMARCRecord -DomainName 'evotec.pl', 'evotec.xyz' -DnsServer 1.1.1.1
+        $DNS.Count | Should -Be 2
+        $DNS[0].Name | Should -Be 'evotec.pl'
+        $DNS[0].DMARC | Should -Be 'v=DMARC1;p=none;rua=mailto:dmarc_agg@vali.email;sp=none;aspf=s;'
+        $DNS[1].Name | Should -Be 'evotec.xyz'
+        $DNS[1].DMARC | Should -Be ''
+        $DNS[0].QueryServer | Should -Be '1.1.1.1:53'
+        $DNS[1].QueryServer | Should -Be '1.1.1.1:53'
+    }
+    It 'Given 2 domains it should return two records using HTTPS GOOGLE' {
+        $DNS = Find-DMARCRecord -DomainName 'evotec.pl', 'evotec.xyz' -DNSProvider Google
+        $DNS.Count | Should -Be 2
+        $DNS[0].Name | Should -Be 'evotec.pl'
+        $DNS[0].DMARC | Should -Be 'v=DMARC1;p=none;rua=mailto:dmarc_agg@vali.email;sp=none;aspf=s;'
+        $DNS[1].Name | Should -Be 'evotec.xyz'
+        $DNS[1].DMARC | Should -Be ''
+        $DNS[0].QueryServer | Should -Be 'dns.google.com'
+        $DNS[1].QueryServer | Should -Be 'dns.google.com'
+    }
+    It 'Given 2 domains it should return two records using HTTPS Cloudflare' {
+        $DNS = Find-DMARCRecord -DomainName 'evotec.pl', 'evotec.xyz' -DNSProvider Cloudflare
+        $DNS.Count | Should -Be 2
+        $DNS[0].Name | Should -Be 'evotec.pl'
+        $DNS[0].DMARC | Should -Be 'v=DMARC1;p=none;rua=mailto:dmarc_agg@vali.email;sp=none;aspf=s;'
+        $DNS[1].Name | Should -Be 'evotec.xyz'
+        $DNS[1].DMARC | Should -Be ''
+        $DNS[0].QueryServer | Should -Be 'cloudflare-dns.com'
+        $DNS[1].QueryServer | Should -Be 'cloudflare-dns.com'
+    }
+}
