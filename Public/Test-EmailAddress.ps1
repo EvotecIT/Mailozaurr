787258cff41965ea783333a4d30fc98ddac807b0
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-10-25 14:30:51 +0100

    Added some docs

--- Public/Test-EmailAddress.ps1
+++ Public/Test-EmailAddress.ps1
@@ -1,4 +1,26 @@
 ﻿function Test-EmailAddress {
+    <#
+    .SYNOPSIS
+    Checks if email address matches conditions to be valid email address.
+
+    .DESCRIPTION
+    Checks if email address matches conditions to be valid email address.
+
+    .PARAMETER EmailAddress
+    EmailAddress to check
+
+    .EXAMPLE
+    Test-EmailAddress -EmailAddress 'przemyslaw.klys@test'
+
+    .EXAMPLE
+    Test-EmailAddress -EmailAddress 'przemyslaw.klys@test.pl'
+
+    .EXAMPLE
+    Test-EmailAddress -EmailAddress 'przemyslaw.klys@test','przemyslaw.klys@test.pl'
+
+    .NOTES
+    General notes
+    #>
     [cmdletBinding()]
     param(
         [Parameter(Mandatory, ValueFromPipelineByPropertyName, ValueFromPipeline, Position = 0)][string[]] $EmailAddress
