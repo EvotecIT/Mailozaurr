d8c289ae27bc8ede9c5b021fc7c186f5fa30bc38
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-06-13 09:18:27 +0200

    PSM1 update

--- Mailozaurr.psm1
+++ Mailozaurr.psm1
@@ -2,8 +2,7 @@
 $Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue -Recurse )
 $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue -Recurse )
 
-$AssemblyFolders = Get-ChildItem -Path $PSScriptRoot\Lib -Directory
-
+$AssemblyFolders = Get-ChildItem -Path $PSScriptRoot\Lib -Directory -ErrorAction SilentlyContinue
 if ($AssemblyFolders.BaseName -contains 'Standard') {
     $Assembly = @( Get-ChildItem -Path $PSScriptRoot\Lib\Standard\*.dll -ErrorAction SilentlyContinue )
 } else {
@@ -13,27 +12,43 @@ if ($AssemblyFolders.BaseName -contains 'Standard') {
         $Assembly = @( Get-ChildItem -Path $PSScriptRoot\Lib\Default\*.dll -ErrorAction SilentlyContinue )
     }
 }
-Foreach ($Import in @($Assembly)) {
-    try {
-        Add-Type -Path $Import.Fullname -ErrorAction Stop
-    } catch [System.Reflection.ReflectionTypeLoadException] {
-        Write-Error -Message "Message: $($_.Exception.Message)"
-        Write-Error -Message "StackTrace: $($_.Exception.StackTrace)"
-        Write-Error -Message "LoaderExceptions: $($_.Exception.LoaderExceptions)"
-    } catch {
-        Write-Error -Message "Message: $($_.Exception.Message)"
-        Write-Error -Message "StackTrace: $($_.Exception.StackTrace)"
-        Write-Error -Message "LoaderExceptions: $($_.Exception.LoaderExceptions)"
+$FoundErrors = @(
+    Foreach ($Import in @($Assembly)) {
+        try {
+            Add-Type -Path $Import.Fullname -ErrorAction Stop
+        } catch [System.Reflection.ReflectionTypeLoadException] {
+            Write-Warning "Processing $($Import.Name) Exception: $($_.Exception.Message)"
+            $LoaderExceptions = $($_.Exception.LoaderExceptions) | Sort-Object -Unique
+            foreach ($E in $LoaderExceptions) {
+                Write-Warning "Processing $($Import.Name) LoaderExceptions: $($E.Message)"
+            }
+            $true
+            #Write-Error -Message "StackTrace: $($_.Exception.StackTrace)"
+        } catch {
+            Write-Warning "Processing $($Import.Name) Exception: $($_.Exception.Message)"
+            $LoaderExceptions = $($_.Exception.LoaderExceptions) | Sort-Object -Unique
+            foreach ($E in $LoaderExceptions) {
+                Write-Warning "Processing $($Import.Name) LoaderExceptions: $($E.Message)"
+            }
+            $true
+            #Write-Error -Message "StackTrace: $($_.Exception.StackTrace)"
+        }
     }
-}
+    #Dot source the files
+    Foreach ($Import in @($Private + $Public)) {
+        Try {
+            . $Import.Fullname
+        } Catch {
+            Write-Error -Message "Failed to import functions from $($import.Fullname): $_"
+            $true
+        }
+    }
+)
 
-#Dot source the files
-Foreach ($Import in @($Public + $Private)) {
-    Try {
-        . $Import.Fullname
-    } Catch {
-        Write-Error -Message "Failed to import function $($import.Fullname): $_"
-    }
+if ($FoundErrors.Count -gt 0) {
+    $ModuleName = (Get-ChildItem $PSScriptRoot\*.psd1).BaseName
+    Write-Warning "Importing module $ModuleName failed. Fix errors before continuing."
+    break
 }
 
 Export-ModuleMember -Function '*' -Alias '*'
