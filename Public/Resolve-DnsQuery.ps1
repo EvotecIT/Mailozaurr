ff5e1fd62c6490a34222c5ff9a15999888b0ae00
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-19 18:03:29 +0200

    Direct types use

--- Public/Resolve-DnsQuery.ps1
+++ Public/Resolve-DnsQuery.ps1
@@ -9,11 +9,11 @@ function Resolve-DnsQuery {
     if ($Server) {
         if ($Server -like '*:*') {
             $SplittedServer = $Server.Split(':')
-            [ipaddress] $IpAddress = $SplittedServer[0]
-            $EndPoint = [IPEndpoint]::new($IpAddress, $SplittedServer[1]) ##(IPAddress.Parse("127.0.0.1"), 8600);
+            [System.Net.IPAddress] $IpAddress = $SplittedServer[0]
+            $EndPoint = [System.Net.IPEndPoint]::new($IpAddress, $SplittedServer[1]) ##(IPAddress.Parse("127.0.0.1"), 8600);
         } else {
-            [ipaddress] $IpAddress = $Server
-            $EndPoint = [IPEndpoint]::new($IpAddress, 53) ##(IPAddress.Parse("127.0.0.1"), 8600);
+            [System.Net.IPAddress] $IpAddress = $Server
+            $EndPoint = [System.Net.IPEndPoint]::new($IpAddress, 53) ##(IPAddress.Parse("127.0.0.1"), 8600);
         }
         $Lookup = [DnsClient.LookupClient]::new($EndPoint)
     } else {
