8385abf0b8e994b342f5305f5a8ac228be5eac7a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-19 18:10:13 +0200

    Verify DLL's

---
+++ Examples/CheckDll.ps1
@@ -0,0 +1,30 @@
+﻿Import-Module $PSScriptRoot\..\Mailozaurr.psd1 -Force
+
+$Type = @(
+    [MailKit.Security.SaslMechanismOAuth2]
+    [MimeKit.MessagePriority]
+    [MimeKit.MimeMessage]
+    [MimeKit.InternetAddress]
+    [MailKit.Security.SecureSocketOptions]
+    [System.Data.DataTable]
+    [System.Text.Encoding]
+    [Net.ServicePointManager]
+    [System.Buffers.BuffersExtensions]
+    [System.Buffers.ArrayPool[int]]
+)
+
+$Output = foreach ($T in $Type) {
+    try {
+        $Item = (Get-Item -LiteralPath ($T).Assembly.Location)
+        [PSCustomObject] @{
+            Name           = $Item.Name
+            FullName       = $Item.FullName
+            FileVersion    = $Item.VersionInfo.FileVersion
+            ProductVersion = $Item.VersionInfo.ProductVersion
+            ProductName    = $Item.VersionInfo.ProductName
+        }
+    } catch {
+        Write-Warning -Message "Couldn't process $T. Exception $($_.Exception.message)"
+    }
+}
+$Output | Format-Table *
