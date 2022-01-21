8ed7bd4ba243defc26e9dae7afd0ed482416aadf
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-01-21 09:23:16 +0100

    Removed comments

--- Private/ConvertTo-MailboxAddress.ps1
+++ Private/ConvertTo-MailboxAddress.ps1
@@ -3,14 +3,6 @@
     param(
         [Array] $MailboxAddress
     )
-    <#
-    MimeKit.MailboxAddress new(System.Text.Encoding encoding, string name, System.Collections.Generic.IEnumerable[string] route, string address)
-    MimeKit.MailboxAddress new(string name, System.Collections.Generic.IEnumerable[string] route, string address)
-    MimeKit.MailboxAddress new(System.Collections.Generic.IEnumerable[string] route, string address)
-    MimeKit.MailboxAddress new(System.Text.Encoding encoding, string name, string address)
-    MimeKit.MailboxAddress new(string name, string address)
-    MimeKit.MailboxAddress new(string address)
-    #>
     foreach ($_ in $MailboxAddress) {
         if ($_ -is [string]) {
             $SmtpTo = [MimeKit.MailboxAddress]::new("$_", "$_")
