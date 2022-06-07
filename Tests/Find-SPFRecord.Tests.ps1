be685d34d67ef847265f12e32d0a1d784d5c1880
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-06-07 08:41:07 +0200

    Fix SPF test

--- Tests/Find-SPFRecord.Tests.ps1
+++ Tests/Find-SPFRecord.Tests.ps1
@@ -3,7 +3,7 @@
         $DNS = Find-SPFRecord -DomainName 'evotec.pl', 'evotec.xyz'
         $DNS.Count | Should -Be 2
         $DNS[0].Name | Should -Be 'evotec.pl'
-        $DNS[0].SPF | Should -Be 'v=spf1 a mx ip4:77.55.95.155 ip4:37.59.176.138 ip4:37.59.176.137 include:spf.protection.outlook.com -all'
+        $DNS[0].SPF | Should -Be 'v=spf1 a mx include:spf.protection.outlook.com -all'
         $DNS[1].Name | Should -Be 'evotec.xyz'
         $DNS[1].SPF | Should -Be 'v=spf1 include:spf.protection.outlook.com -all'
     }
@@ -11,7 +11,7 @@
         $DNS = Find-SPFRecord -DomainName 'evotec.pl', 'evotec.xyz' -DnsServer 1.1.1.1
         $DNS.Count | Should -Be 2
         $DNS[0].Name | Should -Be 'evotec.pl'
-        $DNS[0].SPF | Should -Be 'v=spf1 a mx ip4:77.55.95.155 ip4:37.59.176.138 ip4:37.59.176.137 include:spf.protection.outlook.com -all'
+        $DNS[0].SPF | Should -Be 'v=spf1 a mx include:spf.protection.outlook.com -all'
         $DNS[1].Name | Should -Be 'evotec.xyz'
         $DNS[1].SPF | Should -Be 'v=spf1 include:spf.protection.outlook.com -all'
         $DNS[0].QueryServer | Should -Be '1.1.1.1:53'
@@ -21,7 +21,7 @@
         $DNS = Find-SPFRecord -DomainName 'evotec.pl', 'evotec.xyz' -DNSProvider Google
         $DNS.Count | Should -Be 2
         $DNS[0].Name | Should -Be 'evotec.pl'
-        $DNS[0].SPF | Should -Be 'v=spf1 a mx ip4:77.55.95.155 ip4:37.59.176.138 ip4:37.59.176.137 include:spf.protection.outlook.com -all'
+        $DNS[0].SPF | Should -Be 'v=spf1 a mx include:spf.protection.outlook.com -all'
         $DNS[1].Name | Should -Be 'evotec.xyz'
         $DNS[1].SPF | Should -Be 'v=spf1 include:spf.protection.outlook.com -all'
         $DNS[0].QueryServer | Should -Be 'dns.google.com'
@@ -31,7 +31,7 @@
         $DNS = Find-SPFRecord -DomainName 'evotec.pl', 'evotec.xyz' -DNSProvider Cloudflare
         $DNS.Count | Should -Be 2
         $DNS[0].Name | Should -Be 'evotec.pl'
-        $DNS[0].SPF | Should -Be 'v=spf1 a mx ip4:77.55.95.155 ip4:37.59.176.138 ip4:37.59.176.137 include:spf.protection.outlook.com -all'
+        $DNS[0].SPF | Should -Be 'v=spf1 a mx include:spf.protection.outlook.com -all'
         $DNS[1].Name | Should -Be 'evotec.xyz'
         $DNS[1].SPF | Should -Be 'v=spf1 include:spf.protection.outlook.com -all'
         $DNS[0].QueryServer | Should -Be 'cloudflare-dns.com'
