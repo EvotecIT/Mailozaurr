fdd8029003409a400880afe0e7adaac2d8b39c53
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-12-20 13:28:37 +0100

    Bump version to 1.0.0

--- Build/Manage-Mailozaurr.ps1
+++ Build/Manage-Mailozaurr.ps1
@@ -17,7 +17,7 @@ $Configuration = @{
 
         Manifest          = @{
             # Version number of this module.
-            ModuleVersion              = '0.9.X'
+            ModuleVersion              = '1.0.0'
             # Supported PSEditions
             CompatiblePSEditions       = @('Desktop', 'Core')
             # ID used to uniquely identify this module
