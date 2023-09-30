83ab41b6182f4dddc86a6b34bd3dee5a514c2725
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-09-30 18:44:40 +0200

    Add Example-QueryTLSRPT.ps1 and Find-TLSRPTRecord.ps1
    - Added Example-QueryTLSRPT.ps1 which imports the Mailozaurr module and queries TLSRPT records for the domain 'evotec.xyz' using Cloudflare and Google DNS providers.
    - Added Find-TLSRPTRecord.ps1 which contains the Find-TLSRPTRecord function for querying TLSRPT records for a given domain.
    - The function takes parameters such as DomainName, DnsServer, DNSProvider, AsHashTable, and AsObject.
    - The function retrieves and processes TLSRPT records for each domain specified, returning the results in either hashtable or object format.
    - If an error occurs during the query, the function handles it and returns the appropriate error message.
    - The function also includes a comprehensive comment-based help section explaining SMTP TLS Reporting (TLS-RPT) and the purpose of the function.

---
+++ Examples/Example-QueryTLSRPT.ps1
@@ -0,0 +1,6 @@
+﻿Import-Module $PSScriptRoot\..\Mailozaurr.psd1 -Force
+
+# need to verify - cloudflare occassionally fails
+Find-TLSRPTRecord -DomainName 'evotec.xyz' -DNSProvider Cloudflare
+Find-TLSRPTRecord -DomainName 'evotec.xyz' -DNSProvider Google
+Find-TLSRPTRecord -DomainName 'evotec.xyz'
---
+++ Public/Find-TLSRPTRecord.ps1
@@ -0,0 +1,102 @@
+﻿function Find-TLSRPTRecord {
+    <#
+    .SYNOPSIS
+    Short description
+
+    .DESCRIPTION
+
+
+    .PARAMETER DomainName
+    Parameter description
+
+    .PARAMETER DnsServer
+    Parameter description
+
+    .PARAMETER DNSProvider
+    Parameter description
+
+    .PARAMETER AsHashTable
+    Parameter description
+
+    .PARAMETER AsObject
+    Parameter description
+
+    .EXAMPLE
+    An example
+
+    .NOTES
+    SMTP TLS Reporting (TLS-RPT) is a standard that enables the reporting of issues in TLS connectivity
+    that is experienced by applications that send emails and detect misconfigurations.
+    It enables the reporting of email delivery issues that take place when an email isn't encrypted with TLS.
+    In September 2018 the standard was first documented in RFC 8460.
+
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName, ValueFromPipeline, Position = 0)][Array] $DomainName,
+        [string] $DnsServer,
+        [ValidateSet('Cloudflare', 'Google')][string] $DNSProvider,
+        [switch] $AsHashTable,
+        [switch] $AsObject
+    )
+    process {
+        foreach ($Domain in $DomainName) {
+            if ($Domain -is [string]) {
+                $D = $Domain
+            } elseif ($Domain -is [System.Collections.IDictionary]) {
+                $D = $Domain.DomainName
+                if (-not $D) {
+                    Write-Warning 'Find-TLSRPTRecord - property DomainName is required when passing Array of Hashtables'
+                }
+            }
+            $Splat = @{
+                Name        = "_smtp._tls.$D"
+                Type        = 'TXT'
+                ErrorAction = 'Stop'
+            }
+            try {
+                if ($DNSProvider) {
+                    $DNSRecord = Resolve-DnsQueryRest @Splat -All -DNSProvider $DnsProvider
+                } else {
+                    if ($DnsServer) {
+                        $Splat['Server'] = $DnsServer
+                    }
+                    $DNSRecord = Resolve-DnsQuery @Splat -All
+                }
+                [Array] $DNSRecordAnswers = $DNSRecord.Answers | Where-Object Text -Match 'TLSRPTv1'
+                if (-not $AsObject) {
+                    $MailRecord = [ordered] @{
+                        Name        = $D
+                        Count       = $DNSRecordAnswers.Count
+                        TimeToLive  = $DNSRecordAnswers.TimeToLive -join '; '
+                        TLSRPT      = $DNSRecordAnswers.Text -join '; '
+                        QueryServer = $DNSRecord.NameServer -join '; '
+                    }
+                } else {
+                    $MailRecord = [ordered] @{
+                        Name        = $D
+                        Count       = $DNSRecordAnswers.Count
+                        TimeToLive  = $DNSRecordAnswers.TimeToLive
+                        TLSRPT      = $DNSRecordAnswers.Text
+                        QueryServer = $DNSRecord.NameServer
+                    }
+                }
+            } catch {
+                $MailRecord = [ordered] @{
+                    Name        = $D
+                    Count       = 0
+                    TimeToLive  = ''
+                    TLSRPT      = ''
+                    QueryServer = ''
+                }
+                Write-Warning "Find-TLSRPTRecord - $_"
+            }
+            if ($AsHashTable) {
+                $MailRecord
+            } else {
+                [PSCustomObject] $MailRecord
+            }
+        }
+    }
+}
+
