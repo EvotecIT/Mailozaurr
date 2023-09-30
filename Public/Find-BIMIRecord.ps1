0f7885402531139aeb570dddcc28c28dc785d84f
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-09-30 18:55:07 +0200

    Add Example-QueryBIMI.ps1 and Find-BIMIRecord.ps1 files. Example-QueryBIMI.ps1 queries BIMI records using Find-BIMIRecord function for different domain names and DNS providers. The Find-BIMIRecord.ps1 file contains the Find-BIMIRecord function, which queries for BIMI records based on the specified parameters.

---
+++ Public/Find-BIMIRecord.ps1
@@ -0,0 +1,98 @@
+﻿function Find-BIMIRecord {
+    <#
+    .SYNOPSIS
+    Short description
+
+    .DESCRIPTION
+    Long description
+
+    .PARAMETER DomainName
+    Name/DomainName to query for DMARC record
+
+    .PARAMETER DnsServer
+    Allows to choose DNS IP address to ask for DNS query. By default uses system ones.
+
+    .PARAMETER DNSProvider
+    Allows to choose DNS Provider that will be used for HTTPS based DNS query (Cloudlare or Google)
+
+    .PARAMETER AsHashTable
+    Returns Hashtable instead of PSCustomObject
+
+    .PARAMETER AsObject
+    Returns an object rather than string based represantation for name servers (for easier display purposes)
+
+    .EXAMPLE
+    An example
+
+    .NOTES
+    General notes
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName, ValueFromPipeline, Position = 0)][Array] $DomainName,
+        [string] $DnsServer,
+        [ValidateSet('Cloudflare', 'Google')][string] $DNSProvider,
+        [switch] $AsHashTable,
+        [switch] $AsObject
+    )
+    process {
+        foreach ($Domain in $DomainName) {
+            if ($Domain -is [string]) {
+                $D = $Domain
+            } elseif ($Domain -is [System.Collections.IDictionary]) {
+                $D = $Domain.DomainName
+                if (-not $D) {
+                    Write-Warning -Message 'Find-BIMIRecord - property DomainName is required when passing Array of Hashtables'
+                }
+            }
+            $Splat = @{
+                Name        = "default._bimi.$D"
+                Type        = 'TXT'
+                ErrorAction = 'Stop'
+            }
+            try {
+                if ($DNSProvider) {
+                    $DNSRecord = Resolve-DnsQueryRest @Splat -All -DNSProvider $DnsProvider
+                } else {
+                    if ($DnsServer) {
+                        $Splat['Server'] = $DnsServer
+                    }
+                    $DNSRecord = Resolve-DnsQuery @Splat -All
+                }
+                [Array] $DNSRecordAnswers = $DNSRecord.Answers | Where-Object Text -Match 'v=BIMI1;'
+                if (-not $AsObject) {
+                    $MailRecord = [ordered] @{
+                        Name        = $D
+                        Count       = $DNSRecordAnswers.Count
+                        TimeToLive  = $DNSRecordAnswers.TimeToLive -join '; '
+                        BIMI        = $DNSRecordAnswers.Text -join '; '
+                        QueryServer = $DNSRecord.NameServer -join '; '
+                    }
+                } else {
+                    $MailRecord = [ordered] @{
+                        Name        = $D
+                        Count       = $DNSRecordAnswers.Count
+                        TimeToLive  = $DNSRecordAnswers.TimeToLive
+                        BIMI        = $DNSRecordAnswers.Text
+                        QueryServer = $DNSRecord.NameServer
+                    }
+                }
+            } catch {
+                $MailRecord = [ordered] @{
+                    Name        = $D
+                    Count       = 0
+                    TimeToLive  = ''
+                    TLSRPT      = ''
+                    QueryServer = ''
+                }
+                Write-Warning -Message "Find-BIMIRecord - $_"
+            }
+            if ($AsHashTable) {
+                $MailRecord
+            } else {
+                [PSCustomObject] $MailRecord
+            }
+        }
+    }
+}
+
