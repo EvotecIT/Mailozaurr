1d3d642128c9f9435fb5c19a6221bf1d87b30dce
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-10-29 17:42:29 +0100

    Add Find-SecurityTxtRecord function and an example script to query security.txt information from websites.
    The `Find-SecurityTxtRecord` function allows querying a website's security.txt information. It takes a `DomainName` parameter as input and returns an object containing the security.txt details for each domain queried.
    
    Example usage: `Find-SecurityTxtRecord -DomainName 'evotec.xyz', 'evotec.pl', 'google.com', 'facebook.com', 'www.gemini.com' | Format-Table -AutoSize *`

---
+++ Examples/Example-QuerySecutityTXT.ps1
@@ -0,0 +1,4 @@
+﻿Import-Module $PSScriptRoot\..\Mailozaurr.psd1 -Force
+
+# Standard way
+Find-SecurityTxtRecord -DomainName 'evotec.xyz', 'evotec.pl', 'google.com', 'facebook.com', 'www.gemini.com' | Format-Table -AutoSize *
