23e8136480049c71b39117f94277c91335e28b3b
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-01-16 18:02:11 +0100

    Update example

--- Examples/Example-QueryCAARecord.ps1
+++ Examples/Example-QueryCAARecord.ps1
@@ -3,3 +3,7 @@
 Find-CAARecord -DomainName "evotec.pl" -DNSProvider Cloudflare | Format-Table -AutoSize
 Find-CAARecord -DomainName 'evotec.pl' -DNSProvider Google | Format-Table -AutoSize
 Find-CAARecord -DomainName 'evotec.pl' -Verbose | Format-Table -AutoSize
+
+Find-CAARecord -DomainName "mbank.pl" -DNSProvider Cloudflare | Format-Table -AutoSize
+Find-CAARecord -DomainName 'mbank.pl' -DNSProvider Google | Format-Table -AutoSize
+Find-CAARecord -DomainName 'mbank.pl' -Verbose | Format-Table -AutoSize
