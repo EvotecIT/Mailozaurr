f899bfea3da37a66588778f313d711657ee806f9
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-01-16 17:08:26 +0100

    Add example

---
+++ Examples/Example-QueryCAARecord.ps1
@@ -0,0 +1,5 @@
+﻿Import-Module $PSScriptRoot\..\Mailozaurr.psd1 -Force
+
+Find-CAARecord -DomainName "evotec.pl" -DNSProvider Cloudflare | Format-Table -AutoSize
+Find-CAARecord -DomainName 'evotec.pl' -DNSProvider Google | Format-Table -AutoSize
+Find-CAARecord -DomainName 'evotec.pl' -Verbose | Format-Table -AutoSize
