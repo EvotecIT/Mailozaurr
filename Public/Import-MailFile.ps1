da912a3a3a9b1836ecca31b2f2ece6859faff2ff
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-01-29 22:09:26 +0100

    Add Import-MailFile function to load .msg files as PowerShell objects

---
+++ Public/Import-MailFile.ps1
@@ -0,0 +1,28 @@
+﻿function Import-MailFile {
+    <#
+    .SYNOPSIS
+    Load .msg file as PowerShell object
+
+    .DESCRIPTION
+    Load .msg file as PowerShell object
+
+    .PARAMETER InputPath
+    Path to the .msg file
+
+    .EXAMPLE
+    An example
+
+    .NOTES
+    General notes
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)][alias('FilePath', 'Path')][string] $InputPath
+    )
+    if ($InputPath -and (Test-Path -LiteralPath $InputPath)) {
+        $Message = [MsgReader.Outlook.Storage+Message]::new($InputPath)
+        $Message
+    } else {
+        Write-Warning -Message "Import-MailFile - File $FilePath doesn't exist."
+    }
+}
