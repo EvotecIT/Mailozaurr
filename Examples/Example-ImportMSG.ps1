de3ebea5991f88033c3fcba7fcbba0115038dfbb
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-01-29 22:31:53 +0100

    Update example

--- Examples/Example-ImportMSG.ps1
+++ Examples/Example-ImportMSG.ps1
@@ -1,4 +1,7 @@
 ﻿Import-Module $PSScriptRoot\..\Mailozaurr.psd1 -Force
 
 $Msg = Import-MailFile -FilePath "$PSScriptRoot\Input\TestMessage.msg"
-$Msg | Format-List *
+$Msg | Format-Table
+
+$Eml = Import-MailFile -FilePath "$PSScriptRoot\Input\Sample.eml"
+$Eml | Format-Table
