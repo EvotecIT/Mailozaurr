3ac1e8dcd7e897af1f4f08e8624de02a37e7c4b6
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-01-29 22:31:37 +0100

    Refactor Import-MailFile function to support loading .eml files

--- Public/Import-MailFile.ps1
+++ Public/Import-MailFile.ps1
@@ -1,16 +1,21 @@
 ﻿function Import-MailFile {
     <#
     .SYNOPSIS
-    Load .msg file as PowerShell object
+    Load .msg or .eml file as PowerShell object
 
     .DESCRIPTION
-    Load .msg file as PowerShell object
+    Load .msg or .eml file as PowerShell object
 
     .PARAMETER InputPath
     Path to the .msg file
 
     .EXAMPLE
-    An example
+    $Msg = Import-MailFile -FilePath "$PSScriptRoot\Input\TestMessage.msg"
+    $Msg | Format-Table
+
+    .EXAMPLE
+    $Eml = Import-MailFile -FilePath "$PSScriptRoot\Input\Sample.eml"
+    $Eml | Format-Table
 
     .NOTES
     General notes
@@ -20,8 +25,25 @@
         [Parameter(Mandatory)][alias('FilePath', 'Path')][string] $InputPath
     )
     if ($InputPath -and (Test-Path -LiteralPath $InputPath)) {
-        $Message = [MsgReader.Outlook.Storage+Message]::new($InputPath)
-        $Message
+        Try {
+            $Item = Get-Item -LiteralPath $InputPath -ErrorAction Stop
+        } catch {
+            Write-Warning -Message "Import-MailFile - File $FilePath doesn't exist. Error: $_.Exception.Message"
+            return
+        }
+        try {
+            if ($Item.Extension -eq '.msg') {
+                $Message = [MsgReader.Outlook.Storage+Message]::new($InputPath)
+                $Message
+            } elseif ($Item.Extension -eq '.eml') {
+                $Message = [MsgReader.Mime.Message]::Load($InputPath)
+                $Message
+            } else {
+                Write-Warning -Message "Import-MailFile - File $FilePath is not a .msg or .eml file."
+            }
+        } catch {
+            Write-Warning -Message "Import-MailFile - File $FilePath is not a .msg or .eml file or another error occured. Error: $_.Exception.Message"
+        }
     } else {
         Write-Warning -Message "Import-MailFile - File $FilePath doesn't exist."
     }
