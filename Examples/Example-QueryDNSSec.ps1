03c2b0cde39c6e48bcd471a0e03c8d1daf725db6
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-03-02 09:21:24 +0100

    Update DNSSec

---
+++ Examples/Example-QueryDNSSec.ps1
@@ -0,0 +1,10 @@
+﻿Import-Module $PSScriptRoot\..\Mailozaurr.psd1 -Force
+
+# Standard way
+Find-DNSSECRecord -DomainName 'evotec.pl', 'google.com', 'onet.pl' | Format-Table *
+
+# Https way via Cloudflare
+Find-DNSSECRecord -DomainName 'evotec.pl', 'google.com', 'onet.pl' -DNSProvider Cloudflare | Format-Table *
+
+# Https way via Google
+Find-DNSSECRecord -DomainName 'evotec.pl', 'google.com', 'onet.pl' -DNSProvider Google | Format-Table *
