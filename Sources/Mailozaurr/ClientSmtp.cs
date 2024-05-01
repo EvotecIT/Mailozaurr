66ca07ae32feda14720145cb807b8c8d492f9d3a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-05-01 20:40:48 +0200

    Replace Class with native dll

--- Sources/Mailozaurr/ClientSmtp.cs
+++ Sources/Mailozaurr/ClientSmtp.cs
@@ -1,28 +1,17 @@
 ﻿namespace Mailozaurr;
 
 public class ClientSmtp : SmtpClient {
-    public List<string> DeliveryNotificationOption { get; set; } = new List<string>();
+    public List<DeliveryStatusNotification> DeliveryNotificationOption { get; set; } = new List<DeliveryStatusNotification>();
 
     public ClientSmtp() { }
 
     public ClientSmtp(ProtocolLogger protocolLogger) : base(protocolLogger) { }
 
     protected override DeliveryStatusNotification? GetDeliveryStatusNotifications(MimeMessage message, MailboxAddress mailbox) {
-        var output = new List<DeliveryStatusNotification>();
-
-        if (DeliveryNotificationOption.Contains("OnSuccess")) {
-            output.Add(DeliveryStatusNotification.Success);
+        DeliveryStatusNotification combinedOption = 0;
+        foreach (var option in DeliveryNotificationOption) {
+            combinedOption |= option;
         }
-        if (DeliveryNotificationOption.Contains("Delay")) {
-            output.Add(DeliveryStatusNotification.Delay);
-        }
-        if (DeliveryNotificationOption.Contains("OnFailure")) {
-            output.Add(DeliveryStatusNotification.Failure);
-        }
-        if (DeliveryNotificationOption.Contains("Never")) {
-            output.Add(DeliveryStatusNotification.Never);
-        }
-
-        return output.Count > 0 ? (DeliveryStatusNotification?)output[0] : null;
+        return combinedOption;
     }
 }
