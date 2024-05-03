aee920519fc519dc43f5149e93f9b6cf3d6f9459
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-05-03 10:56:41 +0200

    Add validation to email

---
+++ Sources/Mailozaurr/Definitions/ValidatedEmail.cs
@@ -0,0 +1,7 @@
+﻿namespace Mailozaurr;
+
+public class ValidatedEmail {
+    public string EmailAddress { get; set; } = string.Empty;
+    public bool IsValid { get; set; }
+    public string Error { get; set; } = string.Empty;
+}
---
+++ Sources/Mailozaurr/Validator.cs
@@ -0,0 +1,20 @@
+﻿namespace Mailozaurr;
+
+public static class Validator {
+    public static ValidatedEmail ValidateEmail(string emailAddress, bool allowInternational = false, bool allowTopLevelDomains = false) {
+        try {
+            var isValid = EmailValidation.EmailValidator.Validate(emailAddress, allowTopLevelDomains, allowInternational);
+            return new ValidatedEmail {
+                EmailAddress = emailAddress,
+                IsValid = isValid,
+                Error = ""
+            };
+        } catch (Exception ex) {
+            return new ValidatedEmail {
+                EmailAddress = emailAddress,
+                IsValid = false,
+                Error = ex.Message
+            };
+        }
+    }
+}
