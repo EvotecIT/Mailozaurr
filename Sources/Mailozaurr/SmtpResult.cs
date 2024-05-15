1017295e42ea926b0d892aad84ab506d42232be2
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-05-15 21:02:58 +0200

    Encrypt & sign

--- Sources/Mailozaurr/SmtpResult.cs
+++ Sources/Mailozaurr/SmtpResult.cs
@@ -7,6 +7,7 @@ namespace Mailozaurr;
 /// </summary>
 public class SmtpResult {
     public bool Status { get; set; }
+    public EmailAction EmailAction { get; set; }
     public string SentTo { get; set; }
     public string SentFrom { get; set; }
     public string? Message { get; set; }
@@ -18,13 +19,14 @@ public class SmtpResult {
     /// Initializes a new instance of the <see cref="SmtpResult"/> class.
     /// </summary>
     /// <param name="status">if set to <c>true</c> [status].</param>
+    /// <param name="emailAction">The email action.</param>
     /// <param name="sentTo">The sent to.</param>
     /// <param name="sentFrom">The sent from.</param>
     /// <param name="server">The server.</param>
     /// <param name="port">The port.</param>
     /// <param name="timeToExecute">The time to execute.</param>
     /// <param name="outputMessage">The output message.</param>
-    public SmtpResult(bool status, string sentTo, string sentFrom, string server, int port, TimeSpan timeToExecute, string? outputMessage = null) {
+    public SmtpResult(bool status, EmailAction emailAction, string sentTo, string sentFrom, string server, int port, TimeSpan timeToExecute, string? outputMessage = null) {
         Status = status;
         SentTo = sentTo;
         SentFrom = sentFrom;
@@ -32,25 +34,28 @@ public class SmtpResult {
         Port = port;
         TimeToExecute = timeToExecute;
         Message = outputMessage;
+        EmailAction = emailAction;
     }
 
     /// <summary>
     /// Initializes a new instance of the <see cref="SmtpResult"/> class.
     /// </summary>
     /// <param name="status">if set to <c>true</c> [status].</param>
+    /// <param name="emailAction">The email action.</param>
     /// <param name="sentTo">The sent to.</param>
     /// <param name="sentFrom">The sent from.</param>
     /// <param name="server">The server.</param>
     /// <param name="port">The port.</param>
     /// <param name="timeToExecute">The time to execute.</param>
     /// <param name="loggingConfigurator">The logging configurator.</param>
-    public SmtpResult(bool status, string sentTo, string sentFrom, string server, int port, TimeSpan timeToExecute, LoggingConfigurator? loggingConfigurator) {
+    public SmtpResult(bool status, EmailAction emailAction, string sentTo, string sentFrom, string server, int port, TimeSpan timeToExecute, LoggingConfigurator? loggingConfigurator) {
         Status = status;
         SentTo = sentTo;
         SentFrom = sentFrom;
         Server = server;
         Port = port;
         TimeToExecute = timeToExecute;
+        EmailAction = emailAction;
 
         if (loggingConfigurator?.ProtocolLogger != null && loggingConfigurator.LogObject) {
             Message = Encoding.ASCII.GetString(loggingConfigurator.LogStream?.ToArray() ?? Array.Empty<byte>());
