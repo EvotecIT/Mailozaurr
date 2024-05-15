18a865677d0e31aa0b79e67c66fa316fe73aa3f9
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-05-15 17:30:39 +0200

    Improve sending emails

--- Sources/Mailozaurr/Smtp.cs
+++ Sources/Mailozaurr/Smtp.cs
@@ -1,9 +1,14 @@
 ﻿using System.Diagnostics;
+using System.Net;
 using System.Net.Security;
+using System.Runtime.InteropServices;
+using System.Security;
 
 namespace Mailozaurr;
 
 public class Smtp {
+    public LoggingConfigurator? Logging;
+
     public ClientSmtp Client { get; }
 
     public string Subject {
@@ -78,13 +83,19 @@ public class Smtp {
 
     public string LocalDomain {
         get => Client.LocalDomain;
-        set => Client.LocalDomain = value;
+        set {
+            if (value != "") Client.LocalDomain = value;
+        }
     }
 
     public RemoteCertificateValidationCallback ServerCertificateValidationCallback {
         get => Client.ServerCertificateValidationCallback;
         set => Client.ServerCertificateValidationCallback = value;
     }
+
+    public int Port { get; private set; } = 25;
+
+    public string Server { get; private set; } = String.Empty;
 
     public ActionPreference? ErrorAction { get; set; }
 
@@ -93,13 +104,35 @@ public class Smtp {
 
     private readonly Stopwatch stopwatch;
 
-    public Smtp() {
-        Client = new ClientSmtp();
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Smtp"/> class, with optional logging configuration.
+    /// </summary>
+    /// <param name="logging">The logging.</param>
+    public Smtp(LoggingConfigurator? logging = null) {
+        Logging = logging;
+        Client = logging?.ProtocolLogger == null ? new ClientSmtp() : new ClientSmtp(logging.ProtocolLogger);
         stopwatch = Stopwatch.StartNew();
     }
 
-    public Smtp(ProtocolLogger protocolLogger) {
-        Client = new ClientSmtp(protocolLogger);
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Smtp"/> class with logging configuration.
+    /// </summary>
+    /// <param name="logPath">The log path.</param>
+    /// <param name="logConsole">if set to <c>true</c> [log console].</param>
+    /// <param name="logObject">if set to <c>true</c> [log object].</param>
+    /// <param name="logTimestamps">if set to <c>true</c> [log timestamps].</param>
+    /// <param name="logSecrets">if set to <c>true</c> [log secrets].</param>
+    /// <param name="logTimestampsFormat">The log timestamps format.</param>
+    /// <param name="logServerPrefix">The log server prefix.</param>
+    /// <param name="logClientPrefix">The log client prefix.</param>
+    /// <param name="logOverwrite">if set to <c>true</c> [log overwrite].</param>
+    public Smtp(string logPath, bool logConsole, bool logObject, bool logTimestamps, bool logSecrets,
+        string? logTimestampsFormat = null, string? logServerPrefix = null, string? logClientPrefix = null,
+        bool logOverwrite = false) {
+        Logging = new LoggingConfigurator();
+        Logging.ConfigureLogging(logPath, logConsole, logObject, logTimestamps, logSecrets, logTimestampsFormat,
+                       logServerPrefix, logClientPrefix, logOverwrite);
+        Client = Logging.ProtocolLogger == null ? new ClientSmtp() : new ClientSmtp(Logging.ProtocolLogger);
         stopwatch = Stopwatch.StartNew();
     }
 
@@ -108,10 +141,14 @@ public class Smtp {
     }
 
     public void SaveMessage(string path) {
-        Client.SaveMessage(path);
+        if (!string.IsNullOrEmpty(path)) {
+            Client.SaveMessage(path);
+        }
     }
 
     public SmtpResult Connect(string server, int port, SecureSocketOptions secureSocketOptions = SecureSocketOptions.Auto, bool useSsl = false) {
+        Server = server;
+        Port = port;
         try {
             if (useSsl) {
                 // If useSsl is true, use SecureSocketOptions.StartTls, otherwise use whatever is passed in secureSocketOptions
@@ -131,11 +168,83 @@ public class Smtp {
         }
     }
 
+    public SmtpResult Authenticate(ICredentials Credentials, bool isOAuth = false) {
+        try {
+            if (isOAuth) {
+                var networkCredential = Credentials as NetworkCredential;
+                if (networkCredential != null) {
+                    var (userName, token) = Helpers.ConvertFromOAuth2Credential(networkCredential);
+                    var oauth2 = new SaslMechanismOAuth2(userName, token);
+                    Client.Authenticate(oauth2);
+                    Settings.Logger.WriteVerbose($"Send-EmailMessage - Authenticated using OAuth");
+                }
+                Settings.Logger.WriteVerbose($"Send-EmailMessage - Authenticated using OAuth");
+            } else {
+                Client.Authenticate(Credentials);
+                Settings.Logger.WriteVerbose($"Send-EmailMessage - Authenticated using ICredentials");
+            }
+            return new SmtpResult(true, SentTo, SentFrom, Server, Port, stopwatch.Elapsed, Logging);
+        } catch (Exception ex) {
+            Settings.Logger.WriteWarning($"Send-EmailMessage - Error: {ex.Message}");
+            Settings.Logger.WriteWarning($"Send-EmailMessage - Possible issue: OAuth? ({isOAuth} was used), ICredentials? ({Credentials}, was used).");
+            if (ErrorAction == ActionPreference.Stop) {
+                throw;
+            }
+            return new SmtpResult(false, SentTo, SentFrom, Server, Port, stopwatch.Elapsed, ex.Message);
+        }
+    }
+
+    public SmtpResult Authenticate(string username, string password, bool isSecureString) {
+        if (isSecureString) {
+
+            // Convert the encrypted string back to a SecureString
+            SecureString securePassword = SecureStringHelper.Unprotect(password);
+
+            // Convert the SecureString to a plain string
+            IntPtr unmanagedString = IntPtr.Zero;
+            try {
+                unmanagedString = Marshal.SecureStringToGlobalAllocUnicode(securePassword);
+                password = Marshal.PtrToStringUni(unmanagedString);
+            } finally {
+                Marshal.ZeroFreeGlobalAllocUnicode(unmanagedString);
+            }
+        }
+        try {
+            Client.Authenticate(username, password);
+            Settings.Logger.WriteVerbose($"Send-EmailMessage - Authenticated as {username}");
+            return new SmtpResult(true, SentTo, SentFrom, Server, Port, stopwatch.Elapsed, Logging);
+        } catch (Exception ex) {
+            Settings.Logger.WriteWarning($"Send-EmailMessage - Error: {ex.Message}");
+            Settings.Logger.WriteWarning($"Send-EmailMessage - Possible issue: Username? ({username} was used), Password?.");
+            if (ErrorAction == ActionPreference.Stop) {
+                throw;
+            }
+            return new SmtpResult(false, SentTo, SentFrom, Server, Port, stopwatch.Elapsed, ex.Message);
+        }
+    }
+
+    public SmtpResult Send() {
+        try {
+            Client.Send(Message);
+            Settings.Logger.WriteVerbose($"Send-EmailMessage - Sent email to {SentTo}");
+            return new SmtpResult(true, SentTo, SentFrom, Server, Port, stopwatch.Elapsed, Logging);
+        } catch (Exception ex) {
+            Settings.Logger.WriteWarning($"Send-EmailMessage - Error: {ex.Message}");
+            Settings.Logger.WriteWarning($"Send-EmailMessage - Possible issue: Message? ({Message} was used).");
+            if (ErrorAction == ActionPreference.Stop) {
+                throw;
+            }
+            return new SmtpResult(false, SentTo, SentFrom, Server, Port, stopwatch.Elapsed, ex.Message);
+        }
+    }
+
     public void Disconnect() {
+        Client.Disconnect(true);
         stopwatch.Stop();
     }
 
     public void Dispose() {
+        Client.Dispose();
         stopwatch.Stop();
     }
 }
