562c51baf42946408270978acb31a11a822a7633
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-05-15 17:30:06 +0200

    Add helpers

---
+++ Sources/Mailozaurr/Helpers/Helpers.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.Security;
+
+namespace Mailozaurr;
+public static class Helpers {
+    public static (string UserName, string Token) ConvertFromOAuth2Credential(NetworkCredential credential) {
+        return (credential.UserName, credential.Password);
+    }
+
+    public static NetworkCredential ConvertFromPlainText(string userName, string password) {
+        var secStringPassword = new SecureString();
+        foreach (char c in password) {
+            secStringPassword.AppendChar(c);
+        }
+        return new NetworkCredential(userName, secStringPassword);
+    }
+
+}
