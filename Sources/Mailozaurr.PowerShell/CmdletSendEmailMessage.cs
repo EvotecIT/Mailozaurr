26919c29fb86cc08b9f4790c44f7ce6454236c96
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-05-15 17:30:55 +0200

    initial cmdlets

---
+++ Sources/Mailozaurr.PowerShell/CmdletSendEmailMessage.cs
@@ -0,0 +1,21 @@
+﻿namespace Mailozaurr.PowerShell;
+
+[Cmdlet(VerbsCommunications.Send, "EmailMessageTemporary")]
+public sealed class CmdletSendEmailMessage : AsyncPSCmdlet {
+    private InternalLogger logger;
+    private Smtp SmtpClient;
+
+    protected override Task BeginProcessingAsync() {
+        // Initialize the logger to be able to see verbose, warning, debug, error, progress, and information messages.
+        logger = new InternalLogger(false);
+        var internalLoggerPowerShell = new InternalLoggerPowerShell(logger, this.WriteVerbose, this.WriteWarning, this.WriteDebug, this.WriteError, this.WriteProgress, this.WriteInformation);
+
+        SmtpClient = new Smtp();
+
+        return Task.CompletedTask;
+    }
+    protected override async Task ProcessRecordAsync() {
+
+    }
+
+}
---
+++ Sources/Mailozaurr.PowerShell/InitializeSmtp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Management.Automation;
+using System.Threading.Tasks;
+
+namespace Mailozaurr.PowerShell;
+
+public class InitializeSmtp : AsyncPSCmdlet {
+
+    public InitializeSmtp() {
+        // Initialize the logger
+        var internalLogger = new InternalLogger(false);
+        // Initialize the PowerShell logger, and subscribe to the verbose message event
+        var internalLoggerPowerShell = new InternalLoggerPowerShell(internalLogger, this.WriteVerbose, this.WriteWarning, this.WriteDebug, this.WriteError, this.WriteProgress, this.WriteInformation);
+
+        // Initialize the SMTP client
+        var smtpClient = new Smtp();
+    }
+
+}
+
