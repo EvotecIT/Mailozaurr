adb1c0c819ca86e87f251b433cf64f4aaf253ae5
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-05-15 21:03:20 +0200

    Email actions

---
+++ Sources/Mailozaurr/Enums/EmailAction.cs
@@ -0,0 +1,13 @@
+﻿namespace Mailozaurr;
+
+public enum EmailAction {
+    Authenticate,
+    Connect,
+    SMimeSignature,
+    SMimeSignaturePKCS7,
+    SMimeEncrypt,
+    SMimeSignAndEncrypt,
+    PgpSign,
+    PgpEncrypt,
+    Send
+}
