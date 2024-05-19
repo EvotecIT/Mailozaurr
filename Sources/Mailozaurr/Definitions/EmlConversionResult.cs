aa5ebb1d23b16031c0c227310d2d0c90e07d5294
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-05-19 22:02:42 +0200

    Email conversion results

---
+++ Sources/Mailozaurr/Definitions/EmlConversionResult.cs
@@ -0,0 +1,23 @@
+﻿namespace Mailozaurr;
+
+/// <summary>
+/// Eml conversion result
+/// </summary>
+public class EmlConversionResult {
+    /// <summary>
+    /// Eml file path to file that was converted
+    /// </summary>
+    public string EmlFile { get; set; }
+    /// <summary>
+    /// Msg file path to file that was created
+    /// </summary>
+    public string MsgFile { get; set; }
+    /// <summary>
+    /// Status of the conversion
+    /// </summary>
+    public bool Status { get; set; }
+    /// <summary>
+    /// Error message if conversion failed
+    /// </summary>
+    public string Error { get; set; }
+}
