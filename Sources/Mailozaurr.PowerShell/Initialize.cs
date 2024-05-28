88e45d7b0832cefa1c7ae9b6f0b565ec33507802
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-05-28 22:17:36 +0200

    remove dead files

--- Sources/Mailozaurr.PowerShell/Initialize.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Mailozaurr.PowerShell {
-    /// This is a dummy class that is used to initialize the module in PowerShell
-    /// Doesn't do anything at all
-    public class Initialize {
-
-    }
-}
