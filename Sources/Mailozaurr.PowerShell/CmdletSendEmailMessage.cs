c9a19488f99cc32c1e6519113b4a76099cd02b26
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-06-01 18:45:36 +0200

    improvements to send

--- Sources/Mailozaurr.PowerShell/CmdletSendEmailMessage.cs
+++ Sources/Mailozaurr.PowerShell/CmdletSendEmailMessage.cs
@@ -1,10 +1,8 @@
 ﻿namespace Mailozaurr.PowerShell;
 
-[Cmdlet(VerbsCommunications.Send, "EmailMessageTemporary", DefaultParameterSetName = "Compatibility", SupportsShouldProcess = true)]
+[Cmdlet(VerbsCommunications.Send, "EmailMessage", DefaultParameterSetName = "Compatibility", SupportsShouldProcess = true)]
 [CmdletBinding()]
 public sealed class CmdletSendEmailMessage : AsyncPSCmdlet {
-    private Smtp SmtpClient;
-
     [Parameter(Mandatory = true, ParameterSetName = "SecureString")]
     [Parameter(Mandatory = true, ParameterSetName = "oAuth")]
     [Parameter(Mandatory = true, ParameterSetName = "Compatibility")]
@@ -43,7 +41,7 @@ public sealed class CmdletSendEmailMessage : AsyncPSCmdlet {
     [Parameter(Mandatory = false, ParameterSetName = "MgGraphRequest")]
     [Parameter(Mandatory = false, ParameterSetName = "Compatibility")]
     [Parameter(Mandatory = false, ParameterSetName = "SendGrid")]
-    public string[] Cc { get; set; }
+    public object[] Cc { get; set; }
 
     [Parameter(Mandatory = false, ParameterSetName = "DefaultCredentials")]
     [Parameter(Mandatory = false, ParameterSetName = "SecureString")]
@@ -52,7 +50,7 @@ public sealed class CmdletSendEmailMessage : AsyncPSCmdlet {
     [Parameter(Mandatory = false, ParameterSetName = "MgGraphRequest")]
     [Parameter(Mandatory = false, ParameterSetName = "Compatibility")]
     [Parameter(Mandatory = false, ParameterSetName = "SendGrid")]
-    public string[] Bcc { get; set; }
+    public object[] Bcc { get; set; }
 
     [Parameter(Mandatory = false, ParameterSetName = "DefaultCredentials")]
     [Parameter(Mandatory = false, ParameterSetName = "SecureString")]
@@ -61,7 +59,7 @@ public sealed class CmdletSendEmailMessage : AsyncPSCmdlet {
     [Parameter(Mandatory = false, ParameterSetName = "MgGraphRequest")]
     [Parameter(Mandatory = false, ParameterSetName = "Compatibility")]
     [Parameter(Mandatory = false, ParameterSetName = "SendGrid")]
-    public string[] To { get; set; }
+    public object[] To { get; set; }
 
     [Parameter(Mandatory = false, ParameterSetName = "DefaultCredentials")]
     [Parameter(Mandatory = false, ParameterSetName = "SecureString")]
@@ -76,6 +74,7 @@ public sealed class CmdletSendEmailMessage : AsyncPSCmdlet {
     [Parameter(Mandatory = false, ParameterSetName = "SecureString")]
     [Parameter(Mandatory = false, ParameterSetName = "oAuth")]
     [Parameter(Mandatory = false, ParameterSetName = "Compatibility")]
+    [Parameter(Mandatory = false, ParameterSetName = "SendGrid")]
     [Alias("Importance")]
     public MessagePriority Priority { get; set; }
 
@@ -126,7 +125,6 @@ public sealed class CmdletSendEmailMessage : AsyncPSCmdlet {
     [Parameter(Mandatory = false, ParameterSetName = "Compatibility")]
     public SwitchParameter SkipCertificateRevocation { get; set; }
 
-
     [Parameter(Mandatory = false, ParameterSetName = "DefaultCredentials")]
     [Parameter(Mandatory = false, ParameterSetName = "SecureString")]
     [Parameter(Mandatory = false, ParameterSetName = "oAuth")]
@@ -152,7 +150,7 @@ public sealed class CmdletSendEmailMessage : AsyncPSCmdlet {
     [Parameter(Mandatory = false, ParameterSetName = "Compatibility")]
     [Parameter(Mandatory = false, ParameterSetName = "SendGrid")]
     [Alias("TextBody")]
-    public string Text { get; set; }
+    public string[] Text { get; set; }
 
     [Parameter(Mandatory = false, ParameterSetName = "DefaultCredentials")]
     [Parameter(Mandatory = false, ParameterSetName = "SecureString")]
@@ -193,14 +191,14 @@ public sealed class CmdletSendEmailMessage : AsyncPSCmdlet {
     public bool AsSecureString { get; set; }
 
     [Parameter(Mandatory = false, ParameterSetName = "SendGrid")]
-    public bool SendGrid { get; set; }
+    public SwitchParameter SendGrid { get; set; }
 
     [Parameter(Mandatory = false, ParameterSetName = "SendGrid")]
-    public bool SeparateTo { get; set; }
+    public SwitchParameter SeparateTo { get; set; }
 
     [Parameter(Mandatory = false, ParameterSetName = "Graph")]
     [Parameter(Mandatory = false, ParameterSetName = "MgGraphRequest")]
-    public bool DoNotSaveToSentItems { get; set; }
+    public SwitchParameter DoNotSaveToSentItems { get; set; }
 
     [Parameter(Mandatory = false, ParameterSetName = "DefaultCredentials")]
     [Parameter(Mandatory = false, ParameterSetName = "SecureString")]
@@ -209,8 +207,7 @@ public sealed class CmdletSendEmailMessage : AsyncPSCmdlet {
     [Parameter(Mandatory = false, ParameterSetName = "Graph")]
     [Parameter(Mandatory = false, ParameterSetName = "MgGraphRequest")]
     [Parameter(Mandatory = false, ParameterSetName = "SendGrid")]
-    public bool Suppress { get; set; }
-
+    public SwitchParameter Suppress { get; set; }
 
     [Parameter(Mandatory = false)]
     public string LogPath { get; set; }
@@ -251,19 +248,34 @@ public sealed class CmdletSendEmailMessage : AsyncPSCmdlet {
     [Parameter(Mandatory = false, ParameterSetName = "DefaultCredentials")]
     public bool UseDefaultCredentials { get; set; }
 
-    [Parameter(Mandatory = false)]
+    [Parameter(Mandatory = false, ParameterSetName = "DefaultCredentials")]
+    [Parameter(Mandatory = false, ParameterSetName = "SecureString")]
+    [Parameter(Mandatory = false, ParameterSetName = "oAuth")]
+    [Parameter(Mandatory = false, ParameterSetName = "Compatibility")]
     public EmailActionEncryption SignOrEncrypt { get; set; } = EmailActionEncryption.None;
 
-    [Parameter(Mandatory = false)]
+    [Parameter(Mandatory = false, ParameterSetName = "DefaultCredentials")]
+    [Parameter(Mandatory = false, ParameterSetName = "SecureString")]
+    [Parameter(Mandatory = false, ParameterSetName = "oAuth")]
+    [Parameter(Mandatory = false, ParameterSetName = "Compatibility")]
     public string CertificatePath { get; set; }
 
-    [Parameter(Mandatory = false)]
+    [Parameter(Mandatory = false, ParameterSetName = "DefaultCredentials")]
+    [Parameter(Mandatory = false, ParameterSetName = "SecureString")]
+    [Parameter(Mandatory = false, ParameterSetName = "oAuth")]
+    [Parameter(Mandatory = false, ParameterSetName = "Compatibility")]
     public string CertificatePassword { get; set; }
 
-    [Parameter(Mandatory = false)]
+    [Parameter(Mandatory = false, ParameterSetName = "DefaultCredentials")]
+    [Parameter(Mandatory = false, ParameterSetName = "SecureString")]
+    [Parameter(Mandatory = false, ParameterSetName = "oAuth")]
+    [Parameter(Mandatory = false, ParameterSetName = "Compatibility")]
     public bool CertificatePasswordAsSecureString { get; set; }
 
-    [Parameter(Mandatory = false)]
+    [Parameter(Mandatory = false, ParameterSetName = "DefaultCredentials")]
+    [Parameter(Mandatory = false, ParameterSetName = "SecureString")]
+    [Parameter(Mandatory = false, ParameterSetName = "oAuth")]
+    [Parameter(Mandatory = false, ParameterSetName = "Compatibility")]
     public string CertificateThumbprint { get; set; }
 
 
@@ -272,110 +284,143 @@ public sealed class CmdletSendEmailMessage : AsyncPSCmdlet {
         var internalLogger = new InternalLogger();
         var internalLoggerPowerShell = new InternalLoggerPowerShell(internalLogger, this.WriteVerbose, this.WriteWarning, this.WriteDebug, this.WriteError, this.WriteProgress, this.WriteInformation);
         LoggingMessages.Logger = internalLogger;
-
-        SmtpClient = new Smtp(LogPath, LogConsole, LogObject, LogTimestamps, LogSecrets, LogTimeStampsFormat, LogClientPrefix, LogServerPrefix);
-
-        // WriteObject(SmtpClient);
         return Task.CompletedTask;
     }
 
     protected override Task ProcessRecordAsync() {
-        SmtpClient.From = From;
-        SmtpClient.ReplyTo = ReplyTo;
-        SmtpClient.Cc = Cc;
-        SmtpClient.Bcc = Bcc;
-        SmtpClient.To = To;
-        SmtpClient.Subject = Subject;
-        SmtpClient.Priority = Priority;
-
-        //SmtpClient.Encoding = Encoding;
-        SmtpClient.DeliveryNotificationOption = DeliveryNotificationOption;
-        SmtpClient.DeliveryStatusNotificationType = DeliveryStatusNotificationType;
-
-        SmtpClient.CheckCertificateRevocation = !SkipCertificateRevocation;
-        // SmtpClient.SkipCertificateValidation = SkipCertificateValidation;
-        SmtpClient.HtmlBody = string.Join("", HTML);
-        SmtpClient.TextBody = Text;
-
-        SmtpClient.Attachments = Attachment?.ToList();
-        SmtpClient.Timeout = Timeout;
-
-        //ActionPreference errorAction = this.MyInvocation.BoundParameters.ContainsKey("ErrorAction")
-        //    ? (ActionPreference)this.MyInvocation.BoundParameters["ErrorAction"]
-        //    : ActionPreference.Continue;
-
-        ActionPreference errorActionPreference = (ActionPreference)this.SessionState.PSVariable.GetValue("ErrorActionPreference");
-        SmtpClient.ErrorAction = errorActionPreference;
-
-
-
-        //SmtpClient.RequestReadReceipt = RequestReadReceipt;
-        //SmtpClient.RequestDeliveryReceipt = RequestDeliveryReceipt;
-        //SmtpClient.Graph = Graph;
-        //SmtpClient.MgGraphRequest = MgGraphRequest;
-        //SmtpClient.SendGrid = SendGrid;
-        //SmtpClient.SeparateTo = SeparateTo;
-        //SmtpClient.DoNotSaveToSentItems = DoNotSaveToSentItems;
-        // Connect
-        var Status = SmtpClient.Connect(Server, Port, SecureSocketOptions, UseSsl);
-        if (!Status.Status) {
-            if (!Suppress) {
-                WriteObject(Status);
+        if (SendGrid) {
+            SendGridClient sendGrid = new SendGridClient();
+            NetworkCredential networkCredential = new NetworkCredential(Credential.UserName, Credential.Password);
+            sendGrid.Credentials = networkCredential;
+            sendGrid.From = From;
+            if (Bcc != null) sendGrid.Bcc = Bcc.ToList();
+            if (Cc != null) sendGrid.Cc = Cc.ToList();
+            if (To != null) sendGrid.To = To.ToList();
+            sendGrid.ReplyTo = ReplyTo;
+            sendGrid.Subject = Subject;
+            if (Text != null) sendGrid.Text = string.Join("", Text);
+            if (HTML != null) sendGrid.Html = string.Join("", HTML);
+            sendGrid.Priority = Priority;
+            sendGrid.Attachment = Attachment;
+            sendGrid.SeparateTo = SeparateTo;
+            // create JSON message
+            sendGrid.CreateMessage();
+            if (ShouldProcess(sendGrid.SentTo, "Sending email message via SendGrid")) {
+                var result = sendGrid.SendEmailAsync().GetAwaiter().GetResult();
+                if (!Suppress) {
+                    WriteObject(result);
+                }
+            } else {
+                if (!Suppress) {
+                    WriteObject(new SmtpResult(false, EmailAction.Send, sendGrid.SentTo, sendGrid.SentFrom, "SendGridApi", 0, sendGrid.Stopwatch.Elapsed, "", "Email not sent (WhatIf)"));
+                }
             }
-            SmtpClient.Dispose();
-            return Task.CompletedTask;
-        }
 
-        SmtpClient.CreateMessage();
+        } else if (Graph) {
+            //SmtpClient.RequestReadReceipt = RequestReadReceipt;
+            //SmtpClient.RequestDeliveryReceipt = RequestDeliveryReceipt;
+            //SmtpClient.DoNotSaveToSentItems = DoNotSaveToSentItems;
+        } else if (MgGraphRequest) {
+            //SmtpClient.RequestReadReceipt = RequestReadReceipt;
+            //SmtpClient.RequestDeliveryReceipt = RequestDeliveryReceipt;
+            //SmtpClient.DoNotSaveToSentItems = DoNotSaveToSentItems;
+        } else {
+            Smtp SmtpClient = new Smtp(LogPath, LogConsole, LogObject, LogTimestamps, LogSecrets, LogTimeStampsFormat, LogClientPrefix, LogServerPrefix);
+            SmtpClient.From = From;
+            SmtpClient.ReplyTo = ReplyTo;
+            SmtpClient.Cc = Cc;
+            SmtpClient.Bcc = Bcc;
+            SmtpClient.To = To;
+            SmtpClient.Subject = Subject;
+            SmtpClient.Priority = Priority;
 
-        // Sign or Encrypt
-        if (SignOrEncrypt != EmailActionEncryption.None) {
-            if (CertificateThumbprint != null) {
-                Status = SmtpClient.Encrypt(SignOrEncrypt, CertificateThumbprint);
-            } else if (CertificatePath != null && CertificatePassword != null) {
-                Status = SmtpClient.Encrypt(SignOrEncrypt, CertificatePath, CertificatePassword, CertificatePasswordAsSecureString);
-            }
+            //SmtpClient.Encoding = Encoding;
+            SmtpClient.DeliveryNotificationOption = DeliveryNotificationOption;
+            SmtpClient.DeliveryStatusNotificationType = DeliveryStatusNotificationType;
+
+            SmtpClient.CheckCertificateRevocation = !SkipCertificateRevocation;
+            // SmtpClient.SkipCertificateValidation = SkipCertificateValidation;
+            if (HTML != null) SmtpClient.HtmlBody = string.Join("", HTML);
+            if (Text != null) SmtpClient.TextBody = string.Join("", Text);
+
+            SmtpClient.Attachments = Attachment?.ToList();
+            SmtpClient.Timeout = Timeout;
+
+            //ActionPreference errorAction = this.MyInvocation.BoundParameters.ContainsKey("ErrorAction")
+            //    ? (ActionPreference)this.MyInvocation.BoundParameters["ErrorAction"]
+            //    : ActionPreference.Continue;
+
+            ActionPreference errorActionPreference = (ActionPreference)this.SessionState.PSVariable.GetValue("ErrorActionPreference");
+            SmtpClient.ErrorAction = errorActionPreference;
+
+            // Connect
+            var Status = SmtpClient.Connect(Server, Port, SecureSocketOptions, UseSsl);
             if (!Status.Status) {
                 if (!Suppress) {
                     WriteObject(Status);
                 }
+
                 SmtpClient.Dispose();
                 return Task.CompletedTask;
             }
-        }
 
-        // Authenticate
-        if (UseDefaultCredentials) {
-            Status = SmtpClient.AuthenticateDefaultCredentials();
-        } else if (Credential != null) {
-            NetworkCredential networkCredential = new NetworkCredential(Credential.UserName, Credential.Password);
-            Status = SmtpClient.Authenticate(networkCredential, OAuth2);
-        } else {
-            Status = SmtpClient.Authenticate(Username, Password, AsSecureString);
-        }
-        if (!Status.Status) {
-            if (!Suppress) {
-                WriteObject(Status);
+            SmtpClient.CreateMessage();
+
+            // Sign or Encrypt
+            if (SignOrEncrypt != EmailActionEncryption.None) {
+                if (CertificateThumbprint != null) {
+                    Status = SmtpClient.Encrypt(SignOrEncrypt, CertificateThumbprint);
+                } else if (CertificatePath != null && CertificatePassword != null) {
+                    Status = SmtpClient.Encrypt(SignOrEncrypt, CertificatePath, CertificatePassword,
+                        CertificatePasswordAsSecureString);
+                }
+
+                if (!Status.Status) {
+                    if (!Suppress) {
+                        WriteObject(Status);
+                    }
+
+                    SmtpClient.Dispose();
+                    return Task.CompletedTask;
+                }
             }
+
+            // Authenticate
+            if (UseDefaultCredentials) {
+                Status = SmtpClient.AuthenticateDefaultCredentials();
+            } else if (Credential != null) {
+                NetworkCredential networkCredential = new NetworkCredential(Credential.UserName, Credential.Password);
+                Status = SmtpClient.Authenticate(networkCredential, OAuth2);
+            } else {
+                Status = SmtpClient.Authenticate(Username, Password, AsSecureString);
+            }
+
+            if (!Status.Status) {
+                if (!Suppress) {
+                    WriteObject(Status);
+                }
+
+                SmtpClient.Dispose();
+                return Task.CompletedTask;
+            }
+
+            if (ShouldProcess(SmtpClient.SentTo, "Sending email message")) {
+                // Send the message
+                Status = SmtpClient.Send();
+                if (!Suppress) {
+                    WriteObject(Status);
+                }
+
+                // Save the message
+                SmtpClient.SaveMessage(MimeMessagePath);
+            } else {
+                WriteObject(new SmtpResult(false, EmailAction.Send, SmtpClient.SentTo, SmtpClient.SentFrom,
+                    SmtpClient.Server, SmtpClient.Port, SmtpClient.Stopwatch.Elapsed, "", "Email not sent (WhatIf)"));
+            }
+
+            // Disconnect & Dispose
             SmtpClient.Dispose();
-            return Task.CompletedTask;
         }
-
-        if (ShouldProcess(SmtpClient.SentTo, "Sending email message")) {
-            // Send the message
-            Status = SmtpClient.Send();
-            if (!Suppress) {
-                WriteObject(Status);
-            }
-
-            // Save the message
-            SmtpClient.SaveMessage(MimeMessagePath);
-        } else {
-            WriteObject(new SmtpResult(false, EmailAction.Send, SmtpClient.SentTo, SmtpClient.SentFrom, SmtpClient.Server, SmtpClient.Port, SmtpClient.Stopwatch.Elapsed, "", "Email not sent (WhatIf)"));
-        }
-
-        // Disconnect & Dispose
-        SmtpClient.Dispose();
         return Task.CompletedTask;
     }
 }
