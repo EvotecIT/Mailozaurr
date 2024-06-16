6e676e34404ff1ee602be3091a4030a6ff9981b0
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-06-16 09:07:23 +0200

    Separate file for graph errors

---
+++ Sources/Mailozaurr/MicrosoftGraph/GraphErrors.cs
@@ -0,0 +1,28 @@
+﻿namespace Mailozaurr;
+
+public class GraphApiError {
+    [JsonPropertyName("error")]
+    public GraphApiErrorDetail Error { get; set; }
+}
+
+public class GraphApiErrorDetail {
+    [JsonPropertyName("code")]
+    public string Code { get; set; }
+
+    [JsonPropertyName("message")]
+    public string Message { get; set; }
+
+    [JsonPropertyName("innerError")]
+    public GraphApiInnerError InnerError { get; set; }
+}
+
+public class GraphApiInnerError {
+    [JsonPropertyName("request-id")]
+    public string RequestId { get; set; }
+
+    [JsonPropertyName("client-request-id")]
+    public string ClientRequestId { get; set; }
+
+    [JsonPropertyName("date")]
+    public DateTime Date { get; set; }
+}
