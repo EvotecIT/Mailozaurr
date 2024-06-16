60e119a39ab3ef473d2f232de934b6be722d97ef
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-06-16 09:14:35 +0200

    moved classes to separate file

---
+++ Sources/Mailozaurr/MicrosoftGraph/GraphAuthorization.cs
@@ -0,0 +1,14 @@
+﻿namespace Mailozaurr;
+
+public class GraphUploadSessionResult {
+    [JsonPropertyName("uploadUrl")]
+    public string UploadUrl { get; set; }
+}
+
+public class GraphAuthorization {
+    [JsonPropertyName("token_type")]
+    public string TokenType { get; set; }
+
+    [JsonPropertyName("access_token")]
+    public string AccessToken { get; set; }
+}
