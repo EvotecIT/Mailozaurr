ab1b399b1b730c81cf56e87d120172f86b4c6563
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-06-16 09:07:11 +0200

    Separate file for attachment classes

---
+++ Sources/Mailozaurr/MicrosoftGraph/GraphAttachment.cs
@@ -0,0 +1,49 @@
+﻿namespace Mailozaurr;
+
+public class GraphAttachmentItemWrapper {
+    [JsonPropertyName("AttachmentItem")]
+    public GraphAttachmentItem AttachmentItem { get; set; }
+
+    public GraphAttachmentItemWrapper(GraphAttachmentItem attachmentItem) {
+        AttachmentItem = attachmentItem;
+    }
+}
+
+public class GraphAttachmentItem {
+    [JsonPropertyName("attachmentType")]
+    public string AttachmentType { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("size")]
+    public long Size { get; set; }
+
+    public GraphAttachmentItem(string attachmentType, string name, long size) {
+        AttachmentType = attachmentType;
+        Name = name;
+        Size = size;
+    }
+}
+
+public class GraphAttachment {
+    [JsonPropertyName("@odata.type")]
+    public string ODataType { get; set; } = "#microsoft.graph.fileAttachment";
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("contentBytes")]
+    public string ContentBytes { get; set; }
+
+    public static GraphAttachment FromFile(string filePath) {
+        var fileInfo = new FileInfo(filePath);
+        var fileBytes = File.ReadAllBytes(filePath);
+        var fileContentBase64 = Convert.ToBase64String(fileBytes);
+
+        return new GraphAttachment {
+            Name = fileInfo.Name,
+            ContentBytes = fileContentBase64
+        };
+    }
+}
