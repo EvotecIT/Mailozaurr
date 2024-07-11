103e2186acef56497ce6a4557fd9024ff249ede7
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-07-11 21:26:14 +0200

    Update

--- Examples/Example-ConvertFromEmlToMsg.ps1
+++ Examples/Example-ConvertFromEmlToMsg.ps1
@@ -1,8 +1,9 @@
 ﻿Import-Module $PSScriptRoot\..\Mailozaurr.psd1 -Force
 
-$Conversion = ConvertFrom-EmlToMsg -InputPath "$PSScriptRoot\Input\Sample.eml" -OutputPath "$PSScriptRoot\Output\Sample.msg"
-
+$Conversion = ConvertFrom-EmlToMsg -InputPath "$PSScriptRoot\Input\Sample.eml" -OutputFolder "$PSScriptRoot\Output" -Verbose -Force
+$Conversion | Format-Table
 if ($Conversion.Status) {
-    $Msg = Import-MailFile -FilePath $Conversion.OutputPath
-    $Msg | Format-List *
+    $Msg = Import-MailFile -FilePath $Conversion.MsgFile
+    $Msg | Format-Table
+    $Msg.Dispose()
 }
