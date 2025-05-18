992d15f4f3a5a893e41da8cd6f591c09e75fb277
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-05-18 12:59:42 +0200

    feat(cmdlets): ✨ Introduce new PowerShell cmdlets for mail operations
    * Added `GetMailFolderCmdlet` to retrieve mail folders for a specified user.
    * Introduced `GetMailMessageAttachmentCmdlet` to fetch attachments from mail messages.
    * Implemented `SaveMailMessageCmdlet` for saving mail messages to a specified path.
    * These cmdlets enhance the functionality for interacting with Microsoft Graph API in PowerShell.

---
+++ Sources/Mailozaurr.PowerShell/SaveMailMessageCmdlet.cs
@@ -0,0 +1,16 @@
+using System.Management.Automation;
+using Mailozaurr;
+
+namespace Mailozaurr.PowerShell {
+    [Cmdlet(VerbsData.Save, "MailMessage")]
+    public class SaveMailMessageCmdlet : PSCmdlet {
+        [Parameter(Mandatory = true, ValueFromPipeline = true)]
+        public GraphEmailMessage[] Message { get; set; }
+        [Parameter(Mandatory = true)]
+        public string Path { get; set; }
+
+        protected override void ProcessRecord() {
+            MicrosoftGraphUtils.SaveMailMessages(Message, Path);
+        }
+    }
+}
