d1422e106205b55e83abf701e85b4ff6b9a0b6f7
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-06-15 18:08:30 +0200

    fix missing PgpSecretKeyRingBundle namespace

--- Sources/Mailozaurr/Smtp/Smtp.cs
+++ Sources/Mailozaurr/Smtp/Smtp.cs
@@ -5,6 +5,7 @@ using System.Runtime.InteropServices;
 using System.Security;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading;
+using Org.BouncyCastle.Bcpg.OpenPgp;
 
 namespace Mailozaurr;
 
