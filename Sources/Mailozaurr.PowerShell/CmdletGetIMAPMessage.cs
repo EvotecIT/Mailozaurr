2d526f11e4311dad8019e50a38e4d95ce026201f
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-06-15 12:07:28 +0200

    Implement IMAP message retrieval

--- Sources/Mailozaurr.PowerShell/CmdletGetIMAPMessage.cs
+++ Sources/Mailozaurr.PowerShell/CmdletGetIMAPMessage.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
 using System.Management.Automation;
 using System.Threading.Tasks;
 using MailKit;
+using MailKit.Search;
+using MimeKit;
 using Mailozaurr.PowerShell;
 
 namespace Mailozaurr.PowerShell;
@@ -33,15 +36,73 @@ public sealed class CmdletGetIMAPMessage : AsyncPSCmdlet {
     public FolderAccess FolderAccess { get; set; } = FolderAccess.ReadOnly;
 
     /// <summary>
-    /// Opens the inbox folder and prepares the client for message retrieval.
+    /// <para type="description">Specifies the starting sequence number of messages to retrieve.</para>
+    /// </summary>
+    [Parameter(Position = 2)]
+    public int? SequenceStart { get; set; }
+
+    /// <summary>
+    /// <para type="description">Specifies the ending sequence number of messages to retrieve. If not provided, only <c>SequenceStart</c> is fetched.</para>
+    /// </summary>
+    [Parameter(Position = 3)]
+    public int? SequenceEnd { get; set; }
+
+    /// <summary>
+    /// <para type="description">Specifies the starting UID of messages to retrieve.</para>
+    /// </summary>
+    [Parameter(Position = 2)]
+    public uint? UidStart { get; set; }
+
+    /// <summary>
+    /// <para type="description">Specifies the ending UID of messages to retrieve. If not provided, only <c>UidStart</c> is fetched.</para>
+    /// </summary>
+    [Parameter(Position = 3)]
+    public uint? UidEnd { get; set; }
+
+    /// <summary>
+    /// <para type="description">Search query used to match messages to retrieve.</para>
+    /// </summary>
+    [Parameter]
+    public SearchQuery? SearchQuery { get; set; }
+
+    /// <summary>
+    /// Opens the inbox folder and retrieves messages if message parameters are specified.
     /// </summary>
     protected override Task ProcessRecordAsync() {
         if (Client != null) {
-            var folder = Client.Data.Inbox;
+            var folder = Client.Folder ?? Client.Data.Inbox;
             folder.Open(FolderAccess);
             WriteVerbose($"Get-IMAPMessage - Total messages {folder.Count}, Recent messages {folder.Recent}");
-            Client.Folder = folder as MailKit.Net.Imap.ImapFolder;
-            WriteObject(Client);
+
+            List<MimeMessage> messages = new();
+
+            if (SearchQuery != null) {
+                var uids = folder.Search(SearchQuery);
+                foreach (var uid in uids) {
+                    messages.Add(folder.GetMessage(uid));
+                }
+            } else if (UidStart.HasValue) {
+                var uidList = new List<UniqueId>();
+                uint endVal = UidEnd ?? UidStart.Value;
+                for (uint id = UidStart.Value; id <= endVal; id++)
+                    uidList.Add(new UniqueId(id));
+
+                var uids = folder.Search(MailKit.Search.SearchQuery.Uids(uidList));
+                foreach (var uid in uids)
+                    messages.Add(folder.GetMessage(uid));
+            } else if (SequenceStart.HasValue) {
+                int start = SequenceStart.Value;
+                int end = SequenceEnd ?? SequenceStart.Value;
+                for (int i = start; i <= end && i < folder.Count; i++)
+                    messages.Add(folder.GetMessage(i));
+            }
+
+            if (messages.Count > 0) {
+                WriteObject(messages, true);
+            } else {
+                Client.Folder = folder as MailKit.Net.Imap.ImapFolder;
+                WriteObject(Client);
+            }
         } else {
             WriteVerbose("Get-IMAPMessage - Client not connected?");
         }
