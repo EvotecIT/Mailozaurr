edb7a6f8a975010a08961f9f252bdf5327b47ddc
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-06-16 20:06:19 +0200

    fix(graph): compute proper content ranges for uploads

--- Sources/Mailozaurr/MicrosoftGraph/Graph.cs
+++ Sources/Mailozaurr/MicrosoftGraph/Graph.cs
@@ -502,11 +502,13 @@ public class Graph {
         using var fileStream = new FileStream(filePath, FileMode.Open);
         var buffer = new byte[chunkSize];
         int bytesRead;
+        long offset = 0;
         while ((bytesRead = await fileStream.ReadAsync(buffer, 0, buffer.Length)) > 0) {
-            var contentRange = $"bytes 0-{bytesRead - 1}/{fileSize}";
+            var contentRange = $"bytes {offset}-{offset + bytesRead - 1}/{fileSize}";
             var byteArrayContent = new ByteArrayContent(buffer, 0, bytesRead);
             byteArrayContent.Headers.Add("Content-Range", contentRange);
             fileContents.Add(byteArrayContent);
+            offset += bytesRead;
         }
 
         return fileContents;
