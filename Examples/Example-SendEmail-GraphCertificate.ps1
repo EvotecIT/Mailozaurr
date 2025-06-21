40182e7fb250ce01a7a3d0395ed5df585bc47745
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-06-21 16:28:27 +0200

    Use Send-EmailMessage in Graph certificate example

--- Examples/Example-SendEmail-GraphCertificate.ps1
+++ Examples/Example-SendEmail-GraphCertificate.ps1
@@ -5,15 +5,7 @@ $CertPath = 'path-to-your.pfx'
 $CertPassword = 'your-cert-password'
 
 $Credential = ConvertTo-GraphCertificateCredential -ClientId $ClientId -TenantId $TenantId -CertificatePath $CertPath -CertificatePassword $CertPassword
-$Token = $Credential.GetNetworkCredential().Password
 
-$Graph = [Mailozaurr.Graph]::new()
-$Graph.From = 'sender@yourtenant.onmicrosoft.com'
-$Graph.To = 'recipient@example.com'
-$Graph.Subject = 'Certificate Graph Test'
-$Graph.HTML = '<p>Hello from Graph using certificate auth!</p>'
-$Graph.AccessToken = $Token
-$Graph.TokenType = 'Bearer'
-
-$Result = $Graph.SendMessageAsync().GetAwaiter().GetResult()
-$Result
+Send-EmailMessage -From 'sender@yourtenant.onmicrosoft.com' -To 'recipient@example.com' `
+    -Credential $Credential -Subject 'Certificate Graph Test' `
+    -HTML '<p>Hello from Graph using certificate auth!</p>' -Graph -Verbose
