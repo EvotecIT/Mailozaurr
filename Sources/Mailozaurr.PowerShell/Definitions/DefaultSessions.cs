e835209a9709b7276fddc27a5136ebc8628a50bc
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-06-26 08:23:20 +0200

    Use AsyncLocal for default sessions

--- Sources/Mailozaurr.PowerShell/Definitions/DefaultSessions.cs
+++ Sources/Mailozaurr.PowerShell/Definitions/DefaultSessions.cs
@@ -1,5 +1,7 @@
 namespace Mailozaurr.PowerShell;
 
+using System.Threading;
+
 /// <summary>
 /// Provides access to the last successful connection objects used by the module.
 /// These are automatically updated by Connect-EmailGraph, Connect-IMAP and
@@ -7,12 +9,25 @@ namespace Mailozaurr.PowerShell;
 /// explicitly provided.
 /// </summary>
 public static class DefaultSessions {
+    private static readonly AsyncLocal<GraphConnectionInfo?> _graphSession = new();
+    private static readonly AsyncLocal<ImapConnectionInfo?> _imapSession = new();
+    private static readonly AsyncLocal<PopConnectionInfo?> _pop3Session = new();
+
     /// <summary>Last Microsoft Graph connection.</summary>
-    public static GraphConnectionInfo? GraphSession { get; set; }
+    public static GraphConnectionInfo? GraphSession {
+        get => _graphSession.Value;
+        set => _graphSession.Value = value;
+    }
 
     /// <summary>Last IMAP connection.</summary>
-    public static ImapConnectionInfo? ImapSession { get; set; }
+    public static ImapConnectionInfo? ImapSession {
+        get => _imapSession.Value;
+        set => _imapSession.Value = value;
+    }
 
     /// <summary>Last POP3 connection.</summary>
-    public static PopConnectionInfo? Pop3Session { get; set; }
+    public static PopConnectionInfo? Pop3Session {
+        get => _pop3Session.Value;
+        set => _pop3Session.Value = value;
+    }
 }
