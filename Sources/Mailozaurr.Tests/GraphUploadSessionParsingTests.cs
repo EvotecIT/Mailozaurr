27ed592c66911819d28300108afefd5182df15b2
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-07-01 22:17:12 +0200

    Improve upload session parsing

---
+++ Sources/Mailozaurr.Tests/GraphUploadSessionParsingTests.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Reflection;
+using Xunit;
+
+namespace Mailozaurr.Tests;
+
+public class GraphUploadSessionParsingTests
+{
+    [Fact]
+    public void ParseUploadSessionResult_WithoutUploadUrl_Throws()
+    {
+        MethodInfo? method = typeof(Graph).GetMethod(
+            "ParseUploadSessionResult",
+            BindingFlags.NonPublic | BindingFlags.Static);
+        Assert.NotNull(method);
+        var ex = Assert.Throws<TargetInvocationException>(() => method!.Invoke(null, new object[] { "{}" }));
+        Assert.IsType<InvalidOperationException>(ex.InnerException);
+        Assert.Contains("Upload URL not found", ex.InnerException!.Message, StringComparison.Ordinal);
+    }
+}
