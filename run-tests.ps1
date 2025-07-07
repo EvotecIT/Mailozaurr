e60e0c3dfdf9f846568a5eccb926d821a0d97c51
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-07-07 17:44:39 +0200

    Remove run-tests.ps1 script as part of project cleanup.

--- run-tests.ps1
+++
@@ -1,5 +0,0 @@
-$dllPath = Join-Path $PSScriptRoot 'Sources/Mailozaurr.PowerShell/bin/Debug/net8.0/Mailozaurr.PowerShell.dll'
-if (-not (Test-Path $dllPath)) {
-    Write-Error "Compiled module not found at $dllPath. Build the project before running tests." -ErrorAction Stop
-}
-& "$PSScriptRoot/Mailozaurr.Tests.ps1"
