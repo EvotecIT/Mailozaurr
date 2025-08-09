b986c128c9f9f312c3388a776deb3508fd6b94a7
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-08-09 23:49:49 +0200

    Make TryAdd helper internal

--- Sources/Mailozaurr/Helpers/DictionaryExtensions.cs
+++ Sources/Mailozaurr/Helpers/DictionaryExtensions.cs
@@ -1,13 +1,20 @@
 #if NETSTANDARD2_0 || NET472
-namespace System.Collections.Generic;
+using System.Collections.Generic;
 
-public static class DictionaryExtensions {
-    public static bool TryAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue value) {
-        if (dictionary.ContainsKey(key)) {
-            return false;
+namespace Mailozaurr
+{
+    internal static class DictionaryExtensions
+    {
+        public static bool TryAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue value)
+        {
+            if (dictionary.ContainsKey(key))
+            {
+                return false;
+            }
+
+            dictionary[key] = value;
+            return true;
         }
-        dictionary[key] = value;
-        return true;
     }
 }
 #endif
