53706db19452425b5adeedcb984cbc7ed53a1856
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-08-16 09:39:13 +0200

    refactor: ✨ update email body retrieval in example script
    * Removed multiple hardcoded email body paths.
    * Centralized email body retrieval to specific HTML files.
    * Improved clarity and maintainability of the example script.

--- Examples/Example-SendEmail-GraphWithMgRequest.ps1
+++ Examples/Example-SendEmail-GraphWithMgRequest.ps1
@@ -6,18 +6,9 @@ $Body = EmailBody {
     New-HTMLText -Text "This is test of Connect-MGGraph functionality"
 }
 
-$Body = Get-Content -Path "G:/GitHub/HtmlForgeX/HtmlForgeX.Examples/bin/Debug/net8.0/welcome-email.html" -Raw
-$Body = Get-Content -Path "G:/GitHub/HtmlForgeX/HtmlForgeX.Examples/bin/Debug/net8.0/simple-confirmation-email.html" -Raw
-$Body = Get-Content -Path "G:/GitHub/HtmlForgeX/HtmlForgeX.Examples/bin/Debug/net8.0/password-reset-email.html" -Raw
-$Body = Get-Content -Path "G:/GitHub/HtmlForgeX/HtmlForgeX.Examples/bin/Debug/net8.0/order-confirmation-email.html" -Raw
-$Body = Get-Content -Path "G:/GitHub/HtmlForgeX/HtmlForgeX.Examples/bin/Debug/net8.0/newsletter-email.html" -Raw
-$Body = Get-Content -Path "G:/GitHub/HtmlForgeX/HtmlForgeX.Examples/bin/Debug/net8.0/invoice-email.html" -Raw
-$Body = Get-Content -Path "G:/GitHub/HtmlForgeX/HtmlForgeX.Examples/bin/Debug/net8.0/corrected-email-pattern.html" -Raw
-#$Body = Get-Content -Path "G:/GitHub/HtmlForgeX/HtmlForgeX.Examples/bin/Debug/net8.0/direct-email-pattern.html" -Raw
-$Body = Get-Content -Path "G:/GitHub/HtmlForgeX/HtmlForgeX.Examples/bin/Debug/net8.0/layout-demo-1-extra-wide.html" -Raw
-$Body = Get-Content -Path "G:/GitHub/HtmlForgeX/HtmlForgeX.Examples/bin/Debug/net8.0/base64-embedding-demo.html" -Raw
-$Body = Get-Content -Path "G:/GitHub/HtmlForgeX/HtmlForgeX.Examples/bin/Debug/net8.0/improved-consistency-demo.html" -Raw
-
+$Path = "C:\Support\GitHub\HtmlForgeX.Emails\HtmlForgeX.Email.Examples\EmailTableAdvancedStyling.html"
+$Path = "C:/Support/GitHub/HtmlForgeX.Emails/HtmlForgeX.Email.Examples/bin/Debug/net8.0/EmailProductLaunchAnnouncement.html"
+$Body = Get-Content -Path $Path -Raw
 # authorize via Connect-MgGraph with delegated rights or any other supported method
 Connect-MgGraph -Scopes Mail.Send -NoWelcome
 
