c82ac4b8959545ffba14036191bfb6999b65b719
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-09-05 21:05:02 +0200

    Add pending message processing

---
+++ Examples/Example-ProcessPendingMessages.ps1
@@ -0,0 +1,8 @@
+$pending = Join-Path $PSScriptRoot 'pending.json'
+$sent = Join-Path $PSScriptRoot 'sentlog.json'
+$smtp = [Mailozaurr.Smtp]::new()
+$smtp.Server = 'smtp.server'
+$smtp.PendingMessageRepository = [Mailozaurr.FilePendingMessageRepository]::new($pending)
+$smtp.SentMessageRepository = [Mailozaurr.FileSentMessageRepository]::new($sent)
+$smtp.ProcessPendingMessagesAsync().GetAwaiter().GetResult()
+
