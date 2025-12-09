61c340ff87fd2807ee8f25ba4931ab0221277ba4
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-12-09 13:40:12 +0100

    chore(version): 🔖 Bump version to `2.0.2`
    * Updated `VersionPrefix` in project files to reflect the new version.
    * Ensured consistency across all relevant project files.

--- Build/Update-Version.ps1
+++ Build/Update-Version.ps1
@@ -2,4 +2,4 @@
 
 Get-ProjectVersion -Path "C:\Support\GitHub\Mailozaurr\Sources" -ExcludeFolders @('C:\Support\GitHub\Mailozaurr\Module\Artefacts') | Format-Table
 
-Set-ProjectVersion -Path "C:\Support\GitHub\Mailozaurr\Sources" -NewVersion "2.0.1" -Verbose -ExcludeFolders @('C:\Support\GitHub\Mailozaurr\Module\Artefacts') #-WhatIf
+Set-ProjectVersion -Path "C:\Support\GitHub\Mailozaurr\Sources" -NewVersion "2.0.2" -Verbose -ExcludeFolders @('C:\Support\GitHub\Mailozaurr\Module\Artefacts') #-WhatIf
