dcfd543f326f71d88679859135d0921bbebcd77b
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-12-30 11:24:39 +0100

    chore(build): 🛠️ Update module version and pre-release tag
    * Bump `ModuleVersion` to `2.0.1`
    * Change `PreReleaseTag` to `Preview1`
    * Enable publishing to PowerShell Gallery and GitHub
    * Set `RefreshPSD1Only` to `false`

--- Build/Build-Package.ps1
+++ Build/Build-Package.ps1
@@ -1,3 +1,4 @@
 ﻿Clear-Host
 
 Invoke-DotNetReleaseBuild -ProjectPath "$PSScriptRoot\..\Sources\Mailozaurr" -CertificateThumbprint '483292C9E317AA13B07BB7A96AE9D1A5ED9E7703'
+Invoke-DotNetReleaseBuild -ProjectPath "$PSScriptRoot\..\Sources\Mailozaurr.Msg" -CertificateThumbprint '483292C9E317AA13B07BB7A96AE9D1A5ED9E7703'
