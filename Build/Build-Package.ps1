134c80d8bd2a7a3a4053f6ec172c88344ace9e16
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2026-02-13 11:58:12 +0100

    feat(build): 🚀 update project structure and versioning
    * Added `project.build.plan.json` to manage build configurations.
    * Created `project.build.json` for defining project build schema.
    * Introduced `Build-Project.ps1` for streamlined build processes.
    * Updated version numbers to `2.0.5` in `Mailozaurr` and `Mailozaurr.Msg` projects.
    * Removed obsolete scripts: `Build-Package.ps1`, `Publish-PackageGitHub.ps1`, `Publish-PackageNuget.ps1`, and `Update-Version.ps1`.

--- Build/Build-Package.ps1
+++
@@ -1,9 +0,0 @@
-﻿Clear-Host
-
-Import-Module PSPublishModule -Force -ErrorAction Stop
-
-$certificateThumbprint = '483292C9E317AA13B07BB7A96AE9D1A5ED9E7703'
-Invoke-DotNetReleaseBuild -ProjectPath @(
-    "$PSScriptRoot\..\Sources\Mailozaurr"
-    "$PSScriptRoot\..\Sources\Mailozaurr.Msg"
-) -CertificateThumbprint $certificateThumbprint
--- Build/Update-Version.ps1
+++
@@ -1,5 +0,0 @@
-﻿Import-Module PSPublishModule -Force
-
-Get-ProjectVersion -Path "C:\Support\GitHub\Mailozaurr\Sources" -ExcludeFolders @('C:\Support\GitHub\Mailozaurr\Module\Artefacts') | Format-Table
-
-Set-ProjectVersion -Path "C:\Support\GitHub\Mailozaurr\Sources" -NewVersion "2.0.4" -Verbose -ExcludeFolders @('C:\Support\GitHub\Mailozaurr\Module\Artefacts') #-WhatIf
