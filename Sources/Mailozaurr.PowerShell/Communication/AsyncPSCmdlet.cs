2f3ca5415bb6e61a801ce3a54c5a428977a36f70
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2026-07-26 01:25:30 +0200

    Make async stream writes cancellation-safe

--- Sources/Mailozaurr.PowerShell/Communication/AsyncPSCmdlet.cs
+++ Sources/Mailozaurr.PowerShell/Communication/AsyncPSCmdlet.cs
@@ -281,7 +281,6 @@ public abstract class AsyncPSCmdlet : PSCmdlet, IDisposable
         if (Volatile.Read(ref _currentOutPipe) is null)
             return;
 
-        ThrowIfStopped();
         _ = TryQueue(new PipelineItem(
             sendToPipeline,
             enumerateCollection ? PipelineType.OutputEnumerate : PipelineType.Output));
@@ -300,7 +299,6 @@ public abstract class AsyncPSCmdlet : PSCmdlet, IDisposable
         if (Volatile.Read(ref _currentOutPipe) is null)
             return;
 
-        ThrowIfStopped();
         _ = TryQueue(new PipelineItem(errorRecord, PipelineType.Error));
     }
 
@@ -337,7 +335,6 @@ public abstract class AsyncPSCmdlet : PSCmdlet, IDisposable
         if (Volatile.Read(ref _currentOutPipe) is null)
             return;
 
-        ThrowIfStopped();
         _ = TryQueue(new PipelineItem(text, PipelineType.Warning));
     }
 
@@ -354,7 +351,6 @@ public abstract class AsyncPSCmdlet : PSCmdlet, IDisposable
         if (Volatile.Read(ref _currentOutPipe) is null)
             return;
 
-        ThrowIfStopped();
         _ = TryQueue(new PipelineItem(text, PipelineType.Verbose));
     }
 
@@ -371,7 +367,6 @@ public abstract class AsyncPSCmdlet : PSCmdlet, IDisposable
         if (Volatile.Read(ref _currentOutPipe) is null)
             return;
 
-        ThrowIfStopped();
         _ = TryQueue(new PipelineItem(text, PipelineType.Debug));
     }
 
@@ -388,7 +383,6 @@ public abstract class AsyncPSCmdlet : PSCmdlet, IDisposable
         if (Volatile.Read(ref _currentOutPipe) is null)
             return;
 
-        ThrowIfStopped();
         _ = TryQueue(new PipelineItem(informationRecord, PipelineType.Information));
     }
 
@@ -405,7 +399,6 @@ public abstract class AsyncPSCmdlet : PSCmdlet, IDisposable
         if (Volatile.Read(ref _currentOutPipe) is null)
             return;
 
-        ThrowIfStopped();
         _ = TryQueue(new PipelineItem((messageData, tags), PipelineType.InformationWithTags));
     }
 
@@ -422,7 +415,6 @@ public abstract class AsyncPSCmdlet : PSCmdlet, IDisposable
         if (Volatile.Read(ref _currentOutPipe) is null)
             return;
 
-        ThrowIfStopped();
         _ = TryQueue(new PipelineItem(progressRecord, PipelineType.Progress));
     }
 
@@ -680,7 +672,7 @@ public abstract class AsyncPSCmdlet : PSCmdlet, IDisposable
 
         Volatile.Write(ref _asyncLifecycleStarted, 1);
         _pipelineThreadId = Environment.CurrentManagedThreadId;
-        _currentOutPipe = outPipe;
+        Volatile.Write(ref _currentOutPipe, outPipe);
 
         var synchronizationContext = SynchronizationContext.Current;
         try
